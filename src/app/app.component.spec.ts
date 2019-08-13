1b15a12b73edaa49d2aaddda6250453e7a77861a
Michael Watzko <Michael.Watzko@stz-softwaretechnik.de>
2019-08-13 15:46:58 +0200

    Rename to winslow-ui-ng

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -16,16 +16,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'emc-ui-ng'`, () => {
+  it(`should have as title 'winslow-ui-ng'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('emc-ui-ng');
+    expect(app.title).toEqual('winslow-ui-ng');
   });
 
   it('should render title in a h1 tag', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('Welcome to emc-ui-ng!');
+    expect(compiled.querySelector('h1').textContent).toContain('Welcome to winslow-ui-ng!');
   });
 });
