bedb4e4f08259d4682e655fd444de25806378352
Michael Watzko <Michael.Watzko@stz-softwaretechnik.de>
2019-08-19 19:31:53 +0200

    Be able to establish a ssh connection with public key and forward input/outputstream to remote addr

--- src/main/java/de/itd/tracking/winslow/Main.java
+++ src/main/java/de/itd/tracking/winslow/Main.java
@@ -2,10 +2,16 @@ package de.itd.tracking.winslow;
 
 import com.hashicorp.nomad.javasdk.NomadApiClient;
 import com.hashicorp.nomad.javasdk.NomadApiConfiguration;
+import de.itd.tracking.winslow.com.AutoMesh;
+import de.itd.tracking.winslow.com.AutoMeshBuilder;
 import de.itd.tracking.winslow.nomad.NomadOrchestrator;
 
+import java.net.SocketException;
+
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws SocketException {
+        AutoMesh mesh = new AutoMeshBuilder().build();
+
         new Winslow(new NomadOrchestrator(new NomadApiClient(new NomadApiConfiguration.Builder().setAddress("http://localhost:4646").build()))).run();
     }
 }
