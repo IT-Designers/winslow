27d1b5661d3252548095dba535872debd0e06841
Michael Watzko <Michael.Watzko@stz-softwaretechnik.de>
2019-08-22 15:19:08 +0200

    Load pipelines and their stages through REST

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,21 +1,26 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
 
-import { AppComponent } from './app.component';
-import { TopbarComponent } from './topbar/topbar.component';
+import {AppComponent} from './app.component';
+import {TopbarComponent} from './topbar/topbar.component';
 import {RouterModule} from '@angular/router';
-import { SystemOverviewComponent } from './system-overview/system-overview.component';
+import {SystemOverviewComponent} from './system-overview/system-overview.component';
+import {PipelinesComponent} from './pipelines/pipelines.component';
+import {HttpClientModule} from '@angular/common/http';
 
 @NgModule({
   declarations: [
     AppComponent,
     TopbarComponent,
-    SystemOverviewComponent
+    SystemOverviewComponent,
+    PipelinesComponent
   ],
   imports: [
+    HttpClientModule,
     BrowserModule,
     RouterModule.forRoot([
       { path: '', component: SystemOverviewComponent},
+      { path: 'pipelines', component: PipelinesComponent }
     ])
   ],
   providers: [],
---
+++ src/app/pipelines/pipelines.component.ts
@@ -0,0 +1,31 @@
+import {Component, OnInit} from '@angular/core';
+import {ApiService, PipelineInfo} from '../api.service';
+
+@Component({
+  selector: 'app-pipelines',
+  templateUrl: './pipelines.component.html',
+  styleUrls: ['./pipelines.component.css']
+})
+export class PipelinesComponent implements OnInit {
+  private pipelines: PipelineInfo[] = [];
+  private stages: Map<string, string[]> = new Map();
+
+  constructor(private api: ApiService) {
+
+  }
+
+  ngOnInit() {
+    this.api.listPipelines().toPromise().then(r => this.pipelines = r);
+  }
+
+  loadStages($event: MouseEvent, pipeline: PipelineInfo) {
+    if ($event.target.dataset.showStages === 'true') {
+      this.api.listStages(pipeline).toPromise().then(s => {
+        this.stages.set(pipeline.id, s);
+        console.log(JSON.stringify(s));
+      });
+    } else {
+      this.stages.delete(pipeline.id);
+    }
+  }
+}
