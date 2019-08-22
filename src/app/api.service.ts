87661f543c39915d7dbca46ba4247aa0cbdcc2a2
Michael Watzko <Michael.Watzko@stz-softwaretechnik.de>
2019-08-22 15:19:08 +0200

    Load pipelines and their stages through REST

---
+++ src/app/api.service.ts
@@ -0,0 +1,45 @@
+import { Injectable } from '@angular/core';
+import {HttpClient} from '@angular/common/http';
+import {environment} from '../environments/environment';
+import {map} from 'rxjs/operators';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ApiService {
+
+
+  constructor(private client: HttpClient) {
+
+  }
+
+  listPipelines() {
+    return this.client.get<PipelineInfo[]>(environment.apiLocation + '/pipelines');
+  }
+
+  listStages(pipeline: PipelineInfo) {
+    return this.client
+      .get<Pipeline[]>(environment.apiLocation + '/stages/' + pipeline.id)
+      .pipe(map(p => {
+        const names: string[] = [];
+        p.forEach(s => names.push(s.name));
+        return names;
+      }));
+  }
+}
+
+export class PipelineInfo {
+  id: string;
+  name: string;
+  desc: string;
+}
+
+export class Pipeline {
+  name: string;
+  desc: string;
+  stages: Stage[];
+}
+
+export class Stage {
+  name: string;
+}
