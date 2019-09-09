a7f0ce206ea21e90801196f6a241bca6df06345b
Michael Watzko <Michael.Watzko@stz-softwaretechnik.de>
2019-09-09 16:06:30 +0200

    Poll for the project state and provide a control to set the next stage to execute

--- src/app/projects/projects.component.ts
+++ src/app/projects/projects.component.ts
@@ -1,7 +1,7 @@
 import {Component, OnDestroy, OnInit} from '@angular/core';
 import {ProjectsCreateDialog} from '../projects-create/projects-create-dialog.component';
 import {MatDialog, MatSnackBar} from '@angular/material';
-import {Project, ProjectApiService} from '../project-api.service';
+import {Project, ProjectApiService, State} from '../project-api.service';
 
 @Component({
   selector: 'app-projects',
@@ -12,16 +12,29 @@ export class ProjectsComponent implements OnInit, OnDestroy {
   projects: Project[];
   interval;
 
-  constructor(private api: ProjectApiService, private createDialog: MatDialog, private snack: MatSnackBar) { }
+  constructor(private api: ProjectApiService, private createDialog: MatDialog, private snack: MatSnackBar) {
+  }
 
   ngOnInit() {
     this.projects = null;
-    this.api.listProjects().toPromise().then(projects => this.projects = projects);
-    this.interval = setInterval(() => {
+    this.api.listProjects().toPromise().then(projects => {
+      this.projects = projects;
+      this.intervalUpdateProjects();
+    });
+    this.interval = setInterval(() => this.intervalUpdateProjects(), 2000);
+  }
+
+  intervalUpdateProjects() {
+    if (this.projects != null) {
       this.projects.forEach(project => {
-        this.api.getProjectState(project.id).toPromise().then(state => project.state = state);
+        this.api.getProjectState(project.id).toPromise().then(state => {
+          if (project.state !== state) {
+            project.state = state;
+            this.loadHistory(project);
+          }
+        });
       });
-    }, 9000);
+    }
   }
 
   ngOnDestroy() {
@@ -33,7 +46,7 @@ export class ProjectsComponent implements OnInit, OnDestroy {
   create() {
     this.createDialog.open(ProjectsCreateDialog, {
       width: '50%',
-      data: {  }
+      data: {}
     }).afterClosed().subscribe(result => {
       console.log(JSON.stringify(result));
       this.api.createProject(result.name, result.pipeline).toPromise().then(result => {
@@ -42,14 +55,31 @@ export class ProjectsComponent implements OnInit, OnDestroy {
     });
   }
 
-  loadHistory(project: Project, load: boolean) {
+  maybeLoadHistory(project: Project, load: boolean, resetHistoryBeforeLoading = true) {
     if (load) {
-      project.history = null;
-      this.api.getProjectHistory(project.id).toPromise().then(history => project.history = history.reverse());
+      if (resetHistoryBeforeLoading) {
+        project.history = null;
+      }
+      this.loadHistory(project);
     }
+  }
+
+  loadHistory(project: Project) {
+    this.api.getProjectHistory(project.id).toPromise().then(history => project.history = history.reverse());
   }
 
   toDate(time: number) {
     return new Date(time).toLocaleString();
+  }
+
+  setNextStage(project: Project, value: string) {
+    this.api.setProjectNextStage(project.id, Number(value)).toPromise().then(result => {
+      this.snack.open('Request has been accepted', 'OK');
+      project.nextStage = Number(value);
+      project.forceProgressOnce = true;
+      this.intervalUpdateProjects();
+    }).catch(error => {
+      this.snack.open('Request failed: ' + error);
+    });
   }
 }
