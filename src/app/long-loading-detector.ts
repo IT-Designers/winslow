8bf11acf8fa6bf41db8be68d7e0a9937e88f0a95
Michael Watzko <Michael.Watzko@stz-softwaretechnik.de>
2019-09-12 15:19:24 +0200

    Move ProjectView into its own component&cleanup, show loading indicator for long requests, properly load only displayed data

---
+++ src/app/long-loading-detector.ts
@@ -0,0 +1,28 @@
+export class LongLoadingDetector {
+  loadingCount: number = null;
+  loadingSince: number = null;
+  longLoadingMs: number = null;
+
+  constructor(msForCategorizationAsLongLoading = 250) {
+    this.loadingCount = 0;
+    this.longLoadingMs = msForCategorizationAsLongLoading;
+  }
+
+  increase() {
+    this.loadingCount += 1;
+    if (this.loadingSince == null) {
+      this.loadingSince = new Date().getTime();
+    }
+  }
+
+  decrease() {
+    this.loadingCount -= 1;
+    if (this.loadingCount <= 0) {
+      this.loadingSince = null;
+    }
+  }
+
+  isLongLoading() {
+    return this.loadingSince != null && this.loadingSince + this.longLoadingMs <= new Date().getTime();
+  }
+}
