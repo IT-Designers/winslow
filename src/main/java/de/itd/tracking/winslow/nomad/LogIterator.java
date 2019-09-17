3f0888b1a87aad42df34415fc9de974b027f9c1d
Michael Watzko <Michael.Watzko@stz-softwaretechnik.de>
2019-09-17 10:40:55 +0200

    Use BufferedReader instead of FramedStream

--- src/main/java/de/itd/tracking/winslow/nomad/LogIterator.java
+++ src/main/java/de/itd/tracking/winslow/nomad/LogIterator.java
@@ -1,13 +1,14 @@
 package de.itd.tracking.winslow.nomad;
 
 import com.hashicorp.nomad.apimodel.AllocationListStub;
-import com.hashicorp.nomad.apimodel.StreamFrame;
 import com.hashicorp.nomad.javasdk.ClientApi;
-import com.hashicorp.nomad.javasdk.FramedStream;
 import com.hashicorp.nomad.javasdk.NomadException;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.Iterator;
+import java.util.NoSuchElementException;
 import java.util.Optional;
 import java.util.function.Supplier;
 
@@ -19,8 +20,8 @@ public class LogIterator implements Iterator<String> {
     private final ClientApi                              clientApi;
     private final Supplier<Optional<AllocationListStub>> state;
 
-    private FramedStream stream = null;
-    private Iterator<String> next = null;
+    private BufferedReader reader = null;
+    private String nextLine = null;
 
     public LogIterator(String id, String taskName, String logType, ClientApi clientApi, Supplier<Optional<AllocationListStub>> state) {
         this.id        = id;
@@ -28,68 +29,58 @@ public class LogIterator implements Iterator<String> {
         this.logType   = logType;
         this.clientApi = clientApi;
         this.state     = state;
-        this.tryReadNext();
     }
 
     private boolean hasTaskStarted(AllocationListStub allocation) {
-        return NomadOrchestrator.hasTaskStarted(allocation, taskName).orElse(false);
+        return NomadOrchestrator.hasTaskStarted(allocation, taskName).orElse(Boolean.FALSE);
     }
 
     private boolean tryEnsureStream() {
-        if (stream == null) {
+        if (reader == null) {
             return state.get().map(allocation -> {
                 if (hasTaskStarted(allocation)) {
                     try {
-                        stream = clientApi.logsAsFrames(allocation.getId(), taskName, false, logType);
-                        return true;
+                        reader = new BufferedReader(new InputStreamReader(clientApi.logs(allocation.getId(), taskName, false, logType)));
+                        return Boolean.TRUE;
                     } catch (IOException | NomadException e) {
                         e.printStackTrace();
-                        return false;
+                        return Boolean.FALSE;
                     }
                 } else {
-                    return false;
+                    return Boolean.FALSE;
                 }
-            }).orElse(false);
+            }).orElse(Boolean.FALSE);
         } else {
             return true;
         }
     }
 
-    private void tryReadNext() {
-        this.next = null;
-
-        if (!this.tryEnsureStream()) {
-            return;
-        }
-        try {
-            StreamFrame next = stream.nextFrame();
-            if (next != null && next.getData() != null) {
-                this.next = new String(next.getData()).lines().iterator();
-            }
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        try {
-            if (!hasNext() && stream != null) {
-                stream.close();
-                stream = null;
-            }
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-    }
-
     @Override
     public boolean hasNext() {
-        return this.next != null && this.next.hasNext();
+        if (!tryEnsureStream()) {
+            return false;
+        }
+
+        if (this.nextLine != null) {
+            return true;
+        } else {
+            try {
+                this.nextLine = this.reader.readLine();
+                return nextLine != null;
+            } catch (IOException e) {
+                throw new RuntimeException("Failed to read next line", e);
+            }
+        }
     }
 
     @Override
     public String next() {
-        var result = this.next.next();
-        if (!this.next.hasNext()) {
-            this.tryReadNext();
+        if (this.nextLine == null && !this.hasNext()) {
+            throw new NoSuchElementException();
+        } else {
+            var line = this.nextLine;
+            this.nextLine = null;
+            return line;
         }
-        return result;
     }
 }
