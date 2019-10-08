27cbd2c951dfe222943be07a4139230cc0361dc5
Michael Watzko <Michael.Watzko@stz-softwaretechnik.de>
2019-10-08 12:08:39 +0200

    Show StorageInfo

---
+++ src/app/storage-api.service.ts
@@ -0,0 +1,22 @@
+import { Injectable } from '@angular/core';
+import {HttpClient} from '@angular/common/http';
+import {environment} from '../environments/environment';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class StorageApiService {
+
+  constructor(private client: HttpClient) {
+  }
+
+  getAll() {
+    return this.client.get<StorageInfo[]>(`${environment.apiLocation}storage`);
+  }
+}
+
+export class StorageInfo {
+  name: string;
+  bytesUsed: number;
+  bytesFree: number;
+}
--- src/app/system-overview/system-overview.component.ts
+++ src/app/system-overview/system-overview.component.ts
@@ -1,22 +1,58 @@
-import {Component, OnInit, ViewChild} from '@angular/core';
+import {Component, OnDestroy, OnInit, ViewChild} from '@angular/core';
 import {NodeInfo, NodesApiService} from '../nodes-api.service';
 import {NotificationService} from '../notification.service';
+import {StorageApiService} from '../storage-api.service';
 
 @Component({
   selector: 'app-system-overview',
   templateUrl: './system-overview.component.html',
   styleUrls: ['./system-overview.component.css']
 })
-export class SystemOverviewComponent implements OnInit {
+export class SystemOverviewComponent implements OnInit, OnDestroy {
 
   nodes: NodeInfo[] = null;
+  storages: any[] = [];
+  interval = null;
 
-  constructor(private api: NodesApiService, private notification: NotificationService) {
+  constructor(private api: NodesApiService, private storageApi: StorageApiService, private notification: NotificationService) {
   }
 
   ngOnInit() {
     this.api.getAllNodeInfo().toPromise()
       .then(result => this.nodes = result)
       .catch(error => this.notification.error(error));
+    this.updateStorages();
+    this.interval = setInterval(() => this.updateStorages(), 10_000);
+  }
+
+  ngOnDestroy(): void {
+    if (this.interval) {
+      clearInterval(this.interval);
+      this.interval = null;
+    }
+  }
+
+  private updateStorages() {
+    this.storageApi.getAll().toPromise()
+      .then(result => this.storages = SystemOverviewComponent.convertToNgxDataset(result))
+      .catch(error => this.notification.error(error));
+  }
+
+  private static convertToNgxDataset(result) {
+    return result.map(entry => {
+      return {
+        name: entry.name,
+        series: [
+          {
+            name: 'used',
+            value: entry.bytesUsed
+          },
+          {
+            name: 'free',
+            value: entry.bytesFree
+          }
+        ]
+      };
+    });
   }
 }
