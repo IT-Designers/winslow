8e1498ce8730590e84645552d07dc3fa86d571b5
Michael Watzko <Michael.Watzko@stz-softwaretechnik.de>
2019-10-08 14:09:10 +0200

    Cleanup, add server overview (which displays the nodes... again)

---
+++ src/app/server/servers.component.ts
@@ -0,0 +1,23 @@
+import { Component, OnInit } from '@angular/core';
+import {NodeInfo, NodesApiService} from '../nodes-api.service';
+import {StorageApiService} from '../storage-api.service';
+import {NotificationService} from '../notification.service';
+
+@Component({
+  selector: 'app-server',
+  templateUrl: './servers.component.html',
+  styleUrls: ['./servers.component.css']
+})
+export class ServersComponent implements OnInit {
+
+  nodes: NodeInfo[] = null;
+
+  constructor(private api: NodesApiService, private notification: NotificationService) { }
+
+  ngOnInit() {
+    this.api.getAllNodeInfo().toPromise()
+      .then(result => this.nodes = result)
+      .catch(error => this.notification.error(error));
+  }
+
+}
