50ebf4b72e9365d4eef3d6d9fd1cf1c90971a081
Michael Watzko <Michael.Watzko@stz-softwaretechnik.de>
2019-10-10 19:02:10 +0200

    Add entry.sh, nomad.hcl and directive to download nomad while building the docker image

---
+++ entry.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+set -e
+
+export NOMAD_PID_FILE=/run/nomad.pid
+
+echo "  :::: Starting nginx"
+service nginx start
+
+echo "  :::: Starting nomad"
+#start-stop-daemon --start --name nomad --quiet --pidfile $NOMAD_PID_FILE --background --exec /usr/bin/nomad -- agent -config /etc/nomad/nomad.hcl
+start-stop-daemon --start --name nomad --quiet --pidfile $NOMAD_PID_FILE --background --startas /bin/bash -- -c "/usr/bin/nomad agent -config /etc/nomad/nomad.hcl &> /var/log/nomad.log"
+sleep 5
+
+echo "  :::: Preparing Winslow startup env"
+mkdir -p "$WINSLOW_WORK_DIRECTORY"
+
+if [ "$WINSLOW_STORAGE_TYPE" == "nfs" ]; then
+    echo "    :: Preparing NFS Storage"
+    mount.nfs "$WINSLOW_STORAGE_PATH" "$WINSLOW_WORK_DIRECTORY"
+
+elif [ "$WINSLOW_STORAGE_TYPE" -ne "" ]; then
+    echo "Error: nknown Storage Type: $WINSLOW_STORAGE_TYPE"
+    exit 1
+fi
+    
+
+echo "  :::: Starting winslow"
+echo ""
+java -jar /usr/bin/winslow.jar
+
+start-stop-daemon --stop --name nomad --quiet --pidfile $NOMAD_PID_FILE
+
