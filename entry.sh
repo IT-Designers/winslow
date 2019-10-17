aff6a94ed7e37701a949c14574522d6ca46e8e0f
Michael Watzko <Michael.Watzko@stz-softwaretechnik.de>
2019-10-17 15:59:26 +0200

    Explicitly use nfs vers=3

--- entry.sh
+++ entry.sh
@@ -15,7 +15,7 @@ mkdir -p "$WINSLOW_WORK_DIRECTORY"
 
 if [ "$WINSLOW_STORAGE_TYPE" == "nfs" ]; then
     echo "    :: Preparing NFS Storage"
-    mount.nfs -o intr,soft "$WINSLOW_STORAGE_PATH" "$WINSLOW_WORK_DIRECTORY"
+    mount.nfs -o vers=3,intr,soft "$WINSLOW_STORAGE_PATH" "$WINSLOW_WORK_DIRECTORY"
 
 elif [ "$WINSLOW_STORAGE_TYPE" -ne "" ]; then
     echo "Error: nknown Storage Type: $WINSLOW_STORAGE_TYPE"
