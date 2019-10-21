a030104fa8a916f651ce6678218d6fff7f5b832c
Michael Watzko <Michael.Watzko@stz-softwaretechnik.de>
2019-10-21 17:59:29 +0200

    Add create pipeline dialog

--- src/app/pipelines/pipelines.component.ts
+++ src/app/pipelines/pipelines.component.ts
@@ -2,6 +2,8 @@ import {Component, OnInit} from '@angular/core';
 import {PipelineApiService, PipelineInfo} from '../api/pipeline-api.service';
 import {NotificationService} from '../notification.service';
 import {LongLoadingDetector} from '../long-loading-detector';
+import {MatDialog, MatDialogRef} from '@angular/material';
+import {CreatePipelineDialogComponent, CreatePipelineResult} from '../pipeline-create-dialog/create-pipeline-dialog.component';
 
 @Component({
   selector: 'app-pipelines',
@@ -17,70 +19,83 @@ export class PipelinesComponent implements OnInit {
 
   longLoading = new LongLoadingDetector();
 
-  constructor(private api: PipelineApiService, private notification: NotificationService) {
-
+  constructor(
+    private api: PipelineApiService,
+    private notification: NotificationService,
+    private dialog: MatDialog) {
   }
 
   ngOnInit() {
     this.api
-        .getPipelineDefinitions()
-        .then(r => {
-          this.pipelines = r;
-          this.pipelines.forEach(p => this.loadRaw(p.id));
-        });
+      .getPipelineDefinitions()
+      .then(r => {
+        this.pipelines = r;
+        this.pipelines.forEach(p => this.loadRaw(p.id));
+      });
   }
 
   loadRaw(pipeline: string) {
     this.api
-        .getRaw(pipeline)
-        .then(raw => {
-          this.raw.set(pipeline, raw);
-        });
+      .getRaw(pipeline)
+      .then(raw => {
+        this.raw.set(pipeline, raw);
+      });
   }
 
   check(pipeline: string, value: string) {
     this.longLoading.increase();
     this.api
-        .checkPipelineDefinition(value)
-        .then(result => {
-          if (result != null) {
-            this.success.delete(pipeline);
-            this.error.set(pipeline, result);
-          } else {
-            this.error.delete(pipeline);
-            this.success.set(pipeline, 'Looks fine!');
-          }
-        })
-        .catch(error => this.notification.error('Request failed: ' + error))
-        .finally(() => this.longLoading.decrease());
+      .checkPipelineDefinition(value)
+      .then(result => {
+        if (result != null) {
+          this.success.delete(pipeline);
+          this.error.set(pipeline, result);
+        } else {
+          this.error.delete(pipeline);
+          this.success.set(pipeline, 'Looks fine!');
+        }
+      })
+      .catch(error => this.notification.error('Request failed: ' + error))
+      .finally(() => this.longLoading.decrease());
   }
 
   update(pipeline: string, value: string) {
     this.longLoading.increase();
     this.api
-        .updatePipelineDefinition(pipeline, value)
-        .then(result => {
-          if (result != null) {
-            this.success.delete(pipeline);
-            this.error.set(pipeline, result);
-          } else {
-            this.error.delete(pipeline);
-            this.success.set(pipeline, 'Saved!');
-            return this.api
-                .getPipelineDefinition(pipeline)
-                .then(def => {
-                  for (const pipe of this.pipelines) {
-                    if (pipeline === pipe.id) {
-                      // migrate values without replacing the object to avoid the list of pipelines to be rebuilt
-                      Object.keys(def).forEach(key => pipe[key] = def[key]);
-                      this.raw.set(pipeline, value);
-                      break;
-                    }
-                  }
-                });
-          }
-        })
-        .catch(error => this.notification.error('Request failed: ' + error))
-        .finally(() => this.longLoading.decrease());
+      .updatePipelineDefinition(pipeline, value)
+      .then(result => {
+        if (result != null) {
+          this.success.delete(pipeline);
+          this.error.set(pipeline, result);
+        } else {
+          this.error.delete(pipeline);
+          this.success.set(pipeline, 'Saved!');
+          return this.api
+            .getPipelineDefinition(pipeline)
+            .then(def => {
+              for (const pipe of this.pipelines) {
+                if (pipeline === pipe.id) {
+                  // migrate values without replacing the object to avoid the list of pipelines to be rebuilt
+                  Object.keys(def).forEach(key => pipe[key] = def[key]);
+                  this.raw.set(pipeline, value);
+                  break;
+                }
+              }
+            });
+        }
+      })
+      .catch(error => this.notification.error('Request failed: ' + error))
+      .finally(() => this.longLoading.decrease());
+  }
+
+  openCreatePipelineDialog() {
+    const dialog: MatDialogRef<CreatePipelineDialogComponent, CreatePipelineResult> = this.dialog.open(CreatePipelineDialogComponent, {});
+    dialog
+      .afterClosed()
+      .subscribe(result => {
+        if (result) {
+          alert(JSON.stringify(result));
+        }
+      });
   }
 }
