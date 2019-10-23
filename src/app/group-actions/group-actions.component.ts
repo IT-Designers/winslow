7d871883a49938535cfe3a57946b9ce901e400f0
Michael Watzko <Michael.Watzko@stz-softwaretechnik.de>
2019-10-23 17:16:12 +0200

    Add inclusive and exclusive project filters for GroupActions

--- src/app/group-actions/group-actions.component.ts
+++ src/app/group-actions/group-actions.component.ts
@@ -11,13 +11,35 @@ export class GroupActionsComponent implements OnInit {
 
   loadError = null;
   longLoading = new LongLoadingDetector();
-  projects: Project[] = null;
-  
+  projects: Project[];
+  filtered: Project[];
+
 
   constructor(public api: ProjectApiService) {
   }
 
   ngOnInit() {
+    this.longLoading.increase();
+    this.api.listProjects()
+      .toPromise()
+      .then(projects => this.filtered = this.projects = projects)
+      .catch(err => this.longLoading = err)
+      .finally(() => this.longLoading.decrease());
   }
 
+  updateFilter(includeTags: string[], excludeTags: string[]) {
+    this.filtered = this.projects.filter(project => {
+      for (const tag of includeTags) {
+        if (project.tags.indexOf(tag) < 0) {
+          return false;
+        }
+      }
+      for (const tag of excludeTags) {
+        if (project.tags.indexOf(tag) >= 0) {
+          return false;
+        }
+      }
+      return true;
+    });
+  }
 }
