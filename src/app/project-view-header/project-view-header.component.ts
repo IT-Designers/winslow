9da1636f25d2bdc1fec0638a21e68fa98a0a85a3
Michael Watzko <Michael.Watzko@stz-softwaretechnik.de>
2019-10-30 11:03:47 +0100

    Ensure the State is passed to the ViewChild('icon')/StateIconComponent

--- src/app/project-view-header/project-view-header.component.ts
+++ src/app/project-view-header/project-view-header.component.ts
@@ -1,4 +1,4 @@
-import {Component, Input, OnInit, Output, ViewChild} from '@angular/core';
+import {AfterViewInit, Component, Input, OnInit, Output, ViewChild} from '@angular/core';
 import {Project, State} from '../api/project-api.service';
 import {StateIconComponent} from '../state-icon/state-icon.component';
 
@@ -7,7 +7,7 @@ import {StateIconComponent} from '../state-icon/state-icon.component';
   templateUrl: './project-view-header.component.html',
   styleUrls: ['./project-view-header.component.css']
 })
-export class ProjectViewHeaderComponent implements OnInit {
+export class ProjectViewHeaderComponent implements OnInit, AfterViewInit {
 
   @Input() project: Project;
   @Input() pauseReason: string = null;
@@ -18,17 +18,26 @@ export class ProjectViewHeaderComponent implements OnInit {
   @ViewChild('icon', {static: false})
   icon: StateIconComponent;
 
+  state: State = null;
+
   constructor() {
   }
 
   @Input()
   set iconState(value: State) {
+    this.state = value;
     if (this.icon != null) {
       this.icon.state = value;
     }
   }
 
   ngOnInit() {
+  }
+
+  ngAfterViewInit(): void {
+    // the ViewChild('icon') might not be set when receiving the state
+    // so to ensure the state is passed to the ViewChild('icon'), set it again after init
+    this.iconState = this.state;
   }
 
 }
