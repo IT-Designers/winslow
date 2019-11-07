869b68375a9ea998291a8c11633ff2eed25f274f
Michael Watzko <Michael.Watzko@stz-softwaretechnik.de>
2019-11-07 11:22:10 +0100

    Adjust Project constructor to be able to set all attribute at once

--- src/main/java/de/itd/tracking/winslow/project/Project.java
+++ src/main/java/de/itd/tracking/winslow/project/Project.java
@@ -11,20 +11,28 @@ public class Project {
     @Nonnull private final String id;
     @Nonnull private final String owner;
 
-    @Nullable private List<String> groups = new ArrayList<>();
-    @Nullable private List<String> tags   = new ArrayList<>();
+    @Nullable private List<String> groups;
+    @Nullable private List<String> tags;
 
     @Nonnull private PipelineDefinition pipeline;
     @Nonnull private String             name;
 
-    public Project(@Nonnull String id, String owner, @Nonnull PipelineDefinition pipeline) {
-        this(id, owner, pipeline, "");
+    public Project(@Nonnull String id, @Nonnull String owner, @Nonnull PipelineDefinition pipeline) {
+        this(id, owner, null, null, pipeline, "");
     }
 
-    public Project(@Nonnull String id, String owner, @Nonnull PipelineDefinition pipeline, @Nonnull String name) {
+    public Project(
+            @Nonnull String id,
+            @Nonnull String owner,
+            @Nullable List<String> groups,
+            @Nullable List<String> tags,
+            @Nonnull PipelineDefinition pipelineDefinition,
+            @Nonnull String name) {
         this.id       = id;
         this.owner    = owner;
-        this.pipeline = pipeline;
+        this.groups   = groups;
+        this.tags     = tags;
+        this.pipeline = pipelineDefinition;
         this.name     = name;
     }
 
@@ -102,7 +110,7 @@ public class Project {
         return this.tags != null && this.tags.remove(tag);
     }
 
-    public void setTags(@Nullable String...tags) {
+    public void setTags(@Nullable String... tags) {
         if (this.tags == null) {
             this.tags = new ArrayList<>(tags != null ? tags.length : 0);
         } else {
