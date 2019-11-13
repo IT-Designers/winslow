8117d9f5a8cb2d38794e751267969d67207e5ba2
Michael Watzko <Michael.Watzko@stz-softwaretechnik.de>
2019-11-13 12:27:45 +0100

    Add EnvSettingsApiService and display by the server provided global environment variables

--- src/app/system-view/system-view.component.ts
+++ src/app/system-view/system-view.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {LongLoadingDetector} from '../long-loading-detector';
 
 @Component({
   selector: 'app-system-view',
@@ -7,6 +8,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SystemViewComponent implements OnInit {
   selection?: string;
+  longLoading = new LongLoadingDetector();
 
   constructor() { }
 
