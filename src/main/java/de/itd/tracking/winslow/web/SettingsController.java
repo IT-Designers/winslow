6cde3f2c3dd49a4ad781f9d2bfe3e36e073bfc6e
Michael Watzko <Michael.Watzko@stz-softwaretechnik.de>
2019-11-13 12:37:31 +0100

    For now, a user must be SUPER* to access global environment variables

--- src/main/java/de/itd/tracking/winslow/web/SettingsController.java
+++ src/main/java/de/itd/tracking/winslow/web/SettingsController.java
@@ -1,13 +1,16 @@
 package de.itd.tracking.winslow.web;
 
 import de.itd.tracking.winslow.Winslow;
-import de.itd.tracking.winslow.fs.LockBus;
+import de.itd.tracking.winslow.auth.GroupRepository;
+import de.itd.tracking.winslow.auth.User;
+import de.itd.tracking.winslow.auth.UserRepository;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.annotation.Nonnull;
 import java.io.IOException;
 import java.util.Map;
+import java.util.Optional;
 
 @RestController
 public class SettingsController {
@@ -19,7 +22,16 @@ public class SettingsController {
     }
 
     @GetMapping("/settings/global-env")
-    public Map<String, String> getEnvironmentVariables() throws IOException {
-        return this.winslow.getSettingsRepository().getGlobalEnvironmentVariables();
+    public Optional<Map<String, String>> getEnvironmentVariables(@Nonnull User user) throws IOException {
+        if (canUserAccess(user)) {
+            return Optional.of(this.winslow.getSettingsRepository().getGlobalEnvironmentVariables());
+        } else {
+            return Optional.empty();
+        }
+    }
+
+    private static boolean canUserAccess(@Nonnull User user) {
+        return UserRepository.SUPERUSER.equals(user.getName())
+                || user.getGroups().anyMatch(GroupRepository.SUPERGROUP::equals);
     }
 }
