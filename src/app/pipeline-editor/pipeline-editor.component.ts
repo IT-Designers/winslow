d3e10a9ce8ffe8a8aa7e6670a528e06b13423a0d
Michael Watzko <Michael.Watzko@stz-softwaretechnik.de>
2019-11-19 19:01:24 +0100

    Ignore ParseErrors if there is no valid editor initialized yet

--- src/app/pipeline-editor/pipeline-editor.component.ts
+++ src/app/pipeline-editor/pipeline-editor.component.ts
@@ -54,20 +54,22 @@ export class PipelineEditorComponent implements OnInit {
 
   @Input()
   set parseError(parseErrors: ParseError[]) {
-    monaco.editor.setModelMarkers(
-      this.editor.getModel(),
-      'parse-editor',
-      parseErrors.map(e => {
-        return {
-          startLineNumber: e.line,
-          startColumn: e.column,
-          endLineNumber: e.line,
-          endColumn: e.column + 100,
-          message: e.message,
-          severity: monaco.MarkerSeverity.Error
-        } as monaco.editor.IMarkerData;
-      })
-    );
+    if (this.editor != null) {
+      monaco.editor.setModelMarkers(
+        this.editor.getModel(),
+        'parse-editor',
+        parseErrors.map(e => {
+          return {
+            startLineNumber: e.line,
+            startColumn: e.column,
+            endLineNumber: e.line,
+            endColumn: e.column + 100,
+            message: e.message,
+            severity: monaco.MarkerSeverity.Error
+          } as monaco.editor.IMarkerData;
+        })
+      );
+    }
   }
 
   onInit(editor: monaco.editor.IStandaloneCodeEditor) {
