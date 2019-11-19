bf3916d07ce515b92b56a9a3a05f286227e8dc03
Michael Watzko <Michael.Watzko@stz-softwaretechnik.de>
2019-11-19 15:08:41 +0100

    Use Monaco-Editor for Pipline Definition changes

--- src/app/pipeline-editor/pipeline-editor.component.ts
+++ src/app/pipeline-editor/pipeline-editor.component.ts
@@ -1,4 +1,4 @@
-import {Component, EventEmitter, Input, OnInit, Output} from '@angular/core';
+import {Component, EventEmitter, Input, OnInit, Output, ViewChild} from '@angular/core';
 
 @Component({
   selector: 'app-pipeline-editor',
@@ -9,17 +9,49 @@ export class PipelineEditorComponent implements OnInit {
 
   @Input() pipelineId: string;
   @Input() raw: string;
-  @Input() error: string = null;
-  @Input() success: string = null;
+  @Input() errorV: string = null;
+  @Input() successV: string = null;
   @Input() enableOnOthers = false;
 
   @Output() others = new EventEmitter<string>();
   @Output() update = new EventEmitter<string>();
   @Output() check = new EventEmitter<string>();
 
+  editor = null;
+
+
+  editorOptions = {
+    theme: 'vs',
+    language: 'yaml'
+  };
+
   constructor() { }
 
   ngOnInit() {
   }
 
+  @Input()
+  set error(error: string) {
+    this.errorV = error;
+    if (this.editor != null) {
+      setTimeout(() => {
+        this.editor.layout();
+      });
+    }
+  }
+
+  @Input()
+  set success(success: string) {
+    this.successV = success;
+    if (this.editor != null) {
+      setTimeout(() => {
+        this.editor.layout();
+      });
+    }
+  }
+
+  onInit(monaco) {
+    this.editor = monaco;
+    this.editor.layout();
+  }
 }
