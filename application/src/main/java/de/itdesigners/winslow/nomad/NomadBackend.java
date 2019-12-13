da3cc1fb74f3e46ec9630bd1280e3dfabf943340
Michael Watzko <Michael.Watzko@stz-softwaretechnik.de>
2019-12-13 11:38:12 +0100

    No longer dump the stack trace on kill

--- application/src/main/java/de/itdesigners/winslow/nomad/NomadBackend.java
+++ application/src/main/java/de/itdesigners/winslow/nomad/NomadBackend.java
@@ -186,7 +186,6 @@ public class NomadBackend implements Backend {
     @Override
     public void kill(@Nonnull String stage) throws IOException {
         try {
-            Thread.dumpStack();
             getNewJobsApi().deregister(stage).getValue();
         } catch (NomadException e) {
             throw new IOException("Failed to deregister job for " + stage, e);
