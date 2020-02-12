c089938039c4b31a74520162594e4efd2ebc1483
Michael Watzko <michael.watzko@it-designers.de>
2020-02-12 15:44:01 +0100

    Distinguish between action pipelines and non-action pipelines and show action buttons instead of a select&button

--- src/app/api/pipeline-api.service.ts
+++ src/app/api/pipeline-api.service.ts
@@ -2,6 +2,7 @@ import {Injectable} from '@angular/core';
 import {environment} from '../../environments/environment';
 import {HttpClient} from '@angular/common/http';
 import {ImageInfo, ParseError} from './project-api.service';
+import {FileInfo} from './files-api.service';
 
 @Injectable({
   providedIn: 'root'
@@ -31,11 +32,18 @@ export class PipelineApiService {
   }
 
   getPipelineDefinition(pipeline: string) {
-    return this.client.get<PipelineInfo>(PipelineApiService.getUrl(`${pipeline}`)).toPromise();
+    return this.client
+      .get<PipelineInfo>(PipelineApiService.getUrl(`${pipeline}`))
+      .toPromise()
+      .then(info => new PipelineInfo(info));
   }
 
   getPipelineDefinitions() {
-    return this.client.get<PipelineInfo[]>(PipelineApiService.getUrl()).toPromise();
+    return this
+      .client
+      .get<PipelineInfo[]>(PipelineApiService.getUrl())
+      .toPromise()
+      .then(info => info.map(i => new PipelineInfo(i)));
   }
 
   /**
@@ -50,7 +58,11 @@ export class PipelineApiService {
   createPipelineDefinition(name: string) {
     const form = new FormData();
     form.set('name', name);
-    return this.client.put<PipelineInfo>(PipelineApiService.getUrl(`create`), form).toPromise();
+    return this
+      .client
+      .put<PipelineInfo>(PipelineApiService.getUrl(`create`), form)
+      .toPromise()
+      .then(info => new PipelineInfo(info));
   }
 }
 
@@ -61,6 +73,31 @@ export class PipelineInfo {
   desc?: string;
   requiredEnvVariables: string[];
   stages: StageInfo[];
+  markers: string[];
+
+  constructor(info: PipelineInfo) {
+    this.id = info.id;
+    this.name = info.name;
+    this.desc = info.desc;
+    this.requiredEnvVariables = info.requiredEnvVariables;
+    this.stages = info.stages;
+    this.markers = info.markers;
+  }
+
+  hasActionMarker() {
+    for (const marker of this.markers) {
+      const lower = marker.toLowerCase();
+      if (lower.startsWith('action')) {
+        return true;
+      }
+    }
+    return false;
+  }
+
+  hasActionMarkerFor(pipelineName: string) {
+    const markers = this.markers.map(m => m.toLowerCase());
+    return markers.indexOf('action') >= 0 || markers.indexOf('action for ' + pipelineName.toLowerCase()) >= 0;
+  }
 }
 
 export class StageInfo {
