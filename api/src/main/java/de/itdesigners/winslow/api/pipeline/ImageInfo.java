b205e03787c92db71ba69ed8f23075fc9385b469
Michael Watzko <michael.watzko@it-designers.de>
2020-05-25 11:32:24 +0200

    Add api change which is missing in prev commit 3c277a1

--- api/src/main/java/de/itdesigners/winslow/api/pipeline/ImageInfo.java
+++ api/src/main/java/de/itdesigners/winslow/api/pipeline/ImageInfo.java
@@ -1,14 +1,18 @@
 package de.itdesigners.winslow.api.pipeline;
 
 import javax.annotation.Nullable;
+import java.beans.ConstructorProperties;
 
 public class ImageInfo {
 
     @Nullable public final String   name;
     @Nullable public final String[] args;
+    @Nullable public final Integer  shmMegabytes;
 
-    public ImageInfo(@Nullable String name, @Nullable String[] args) {
-        this.name = name;
-        this.args = args;
+    @ConstructorProperties({"name, args, shmSizeMegabytes"})
+    public ImageInfo(@Nullable String name, @Nullable String[] args, @Nullable Integer shmMegabytes) {
+        this.name         = name;
+        this.args         = args;
+        this.shmMegabytes = shmMegabytes;
     }
 }
