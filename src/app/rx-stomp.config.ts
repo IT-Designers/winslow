085b4fc25a6dbe93f9d29d58c072ec97ee50c9e9
Michael Watzko <michael.watzko@it-designers.de>
2020-09-23 11:37:37 +0200

    Prepare RxStompConfig for production (less logging) winslow#84

--- src/app/rx-stomp.config.ts
+++ src/app/rx-stomp.config.ts
@@ -1,4 +1,5 @@
 import { InjectableRxStompConfig } from '@stomp/ng2-stompjs';
+import {environment} from '../environments/environment';
 
 export const RxStompConfig: InjectableRxStompConfig = {
   // Which server?
@@ -12,19 +13,21 @@ export const RxStompConfig: InjectableRxStompConfig = {
   // How often to heartbeat?
   // Interval in milliseconds, set to 0 to disable
   heartbeatIncoming: 0, // Typical value 0 - disabled
-  heartbeatOutgoing: 20000, // Typical value 20000 - every 20 seconds
+  heartbeatOutgoing: environment.production ? 5000 : 1000, // Typical value 20000 - every 20 seconds
 
   // Wait in milliseconds before attempting auto reconnect
   // Set to 0 to disable
   // Typical value 500 (500 milli seconds)
-  reconnectDelay: 750,
+  reconnectDelay: environment.production ? 500 : 750,
 
   // Will log diagnostics on console
   // It can be quite verbose, not recommended in production
   // Skip this key to stop logging to console
   debug: (msg: string): void => {
-    console.log(new Date(), msg);
+    if (!environment.production) {
+      console.log(new Date(), msg);
+    }
   },
 
-  logRawCommunication: true
+  logRawCommunication: !environment.production
 };
