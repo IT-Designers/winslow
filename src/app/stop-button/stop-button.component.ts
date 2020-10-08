ec2903f2edc8a3c15bbbe6d049248034e9a4edd7
Michael Watzko <michael.watzko@it-designers.de>
2020-10-08 17:56:00 +0200

    Closes winslow#145: Move the stop menu-button into its own component and add it to the log view

---
+++ src/app/stop-button/stop-button.component.ts
@@ -0,0 +1,42 @@
+import {Component, Input, OnInit} from '@angular/core';
+import {ProjectApiService, ProjectInfo} from '../api/project-api.service';
+import {DialogService} from '../dialog.service';
+
+@Component({
+  selector: 'app-stop-button',
+  templateUrl: './stop-button.component.html',
+  styleUrls: ['./stop-button.component.css']
+})
+export class StopButtonComponent implements OnInit {
+
+  @Input() project: ProjectInfo = null;
+  @Input() disabled: boolean = false;
+  @Input() showText: boolean = true;
+
+  constructor(private api: ProjectApiService,
+              private dialog: DialogService) {
+  }
+
+  ngOnInit(): void {
+  }
+
+  stop(pause: boolean, stageId: string = null) {
+    if (this.project) {
+      this.dialog.openAreYouSure(
+        `Halt stage of ${this.project.name}`,
+        () => this.api.stopStage(this.project.id, pause, stageId).then()
+      );
+    }
+  }
+
+  kill(stageId: string = null) {
+    if (this.project) {
+      this.dialog.openAreYouSure(
+        `Kill stage of ${this.project.name}`,
+        () => this.api.killStage(this.project.id, stageId).then()
+      );
+    }
+  }
+
+
+}
