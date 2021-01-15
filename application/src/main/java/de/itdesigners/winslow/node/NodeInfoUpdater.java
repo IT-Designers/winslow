c19665a79ed136775aef6469db02d7a5cb0a3db7
Michael Watzko <michael.watzko@it-designers.de>
2021-01-15 21:07:00 +0100

    Allow NodeInfoUpdater to be called from outside the updater thread

--- application/src/main/java/de/itdesigners/winslow/node/NodeInfoUpdater.java
+++ application/src/main/java/de/itdesigners/winslow/node/NodeInfoUpdater.java
@@ -9,36 +9,54 @@ import java.util.logging.Logger;
 
 public class NodeInfoUpdater implements Runnable {
 
-    private static final Logger LOG = Logger.getLogger(NodeInfoUpdater.class.getSimpleName());
+    private static final long   SLEEP_TIME_MS = 1_000;
+    private static final Logger LOG           = Logger.getLogger(NodeInfoUpdater.class.getSimpleName());
 
     @Nonnull private final NodeRepository repository;
     @Nonnull private final Node           node;
 
+    private long waitUntil = 0L;
+
     private NodeInfoUpdater(@Nonnull NodeRepository repository, @Nonnull Node node) {
         this.repository = repository;
-        this.node      = node;
+        this.node       = node;
     }
 
-    public static void spawn(@Nonnull NodeRepository repository, @Nonnull Node node) {
+    public static NodeInfoUpdater spawn(@Nonnull NodeRepository repository, @Nonnull Node node) {
         var updater = new NodeInfoUpdater(repository, node);
         var thread  = new Thread(updater);
         thread.setName(NodeInfoUpdater.class.getSimpleName() + "." + node.getName());
         thread.setDaemon(true);
         thread.start();
+        return updater;
     }
 
+    public synchronized void update() throws IOException {
+        repository.updateNodeInfo(node.loadInfo());
+        waitUntil = System.currentTimeMillis() + SLEEP_TIME_MS;
+    }
+
+    public void updateNoThrows() {
+        try {
+            update();
+        } catch (IOException e) {
+            LOG.log(Level.SEVERE, "Failed to update NodeInfo", e);
+            e.printStackTrace();
+        }
+    }
 
 
     @Override
     public void run() {
         while (true) {
-            try {
-                repository.updateNodeInfo(node.loadInfo());
-            } catch (IOException e) {
-                LOG.log(Level.SEVERE, "Failed to update NodeInfo", e);
-                e.printStackTrace();
-            } finally {
-                LockBus.ensureSleepMs(1_000);
+            var currentTimeMillis = System.currentTimeMillis();
+
+            if (waitUntil < currentTimeMillis) {
+                updateNoThrows();
+            }
+            if (waitUntil >= currentTimeMillis) {
+                var toSleep = waitUntil - currentTimeMillis;
+                LockBus.ensureSleepMs(Math.max(1, toSleep));
             }
         }
     }
