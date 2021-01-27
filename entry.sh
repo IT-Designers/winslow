95198619757e844e7d5435aeb612afd95ad14c48
Michael Watzko <michael.watzko@it-designers.de>
2021-01-27 16:09:13 +0100

    Shrink the disk buffers to a more reasonable size via entry.sh

--- entry.sh
+++ entry.sh
@@ -44,6 +44,20 @@ mkdir -p "$WINSLOW_WORK_DIRECTORY"
 
 if [ "$WINSLOW_STORAGE_TYPE" == "nfs" ]; then
     echo "    :: Preparing NFS Storage"
+
+    function check_set_val() {
+        if [ "$(cat $1)" != "$2" ]; then
+            echo "Writing $2 to $1"
+            echo $2 > $1
+        fi
+    }
+
+    # Shrink the disk buffers to a more reasonable size. See http://lwn.net/Articles/572911/
+    # https://unix.stackexchange.com/questions/149029/pernicious-usb-stick-stall-problem-reverting-workaround-fix/149140
+    check_set_val "/proc/sys/vm/dirty_bytes" $((4*1024*1024))
+    check_set_val "/proc/sys/vm/dirty_background_bytes" $((4*1024*1024))
+
+    
     mkdir -p /run/sendsigs.omit.d/
     service rpcbind start
 #    mount.nfs -o vers=4,intr,soft "$WINSLOW_STORAGE_PATH" "$WINSLOW_WORK_DIRECTORY"
