81e69ffc38892f9fc556955006c8f74bb61c7a38
Michael Watzko <michael.watzko@it-designers.de>
2021-04-28 20:44:43 +0200

    Add REST api to retrieve NodeUtilization reports for a given time span

--- application/src/main/java/de/itdesigners/winslow/web/api/NodesController.java
+++ application/src/main/java/de/itdesigners/winslow/web/api/NodesController.java
@@ -2,10 +2,14 @@ package de.itdesigners.winslow.web.api;
 
 import de.itdesigners.winslow.Winslow;
 import de.itdesigners.winslow.api.node.NodeInfo;
+import de.itdesigners.winslow.api.node.NodeUtilization;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.annotation.Nullable;
+import java.time.Duration;
 import java.util.Optional;
 import java.util.stream.Stream;
 
@@ -36,4 +40,20 @@ public class NodesController {
     public Optional<NodeInfo> getNodeInfo(@PathVariable("name") String name) {
         return winslow.getNodeRepository().getNodeInfo(name);
     }
+
+    @GetMapping("/nodes/{name}/utilization")
+    public Stream<NodeUtilization> getNodeUtilization(
+            @PathVariable("name") String name,
+            @RequestParam(required = false, name = "from") @Nullable Long from,
+            @RequestParam(required = false, name = "to") @Nullable Long to) {
+        return winslow
+                .getNodeRepository()
+                .getNodeUtilizationBetween(
+                        name,
+                        from != null ? from : System.currentTimeMillis() - Duration.ofMinutes(1).toMillis(),
+                        to != null ? to : System.currentTimeMillis()
+                );
+    }
+
+
 }
