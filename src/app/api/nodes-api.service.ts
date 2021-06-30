fb3e71cb4c873aae3c0fbf8649b7884800674b55
Alexander Völker <alexander.voelker@it-designers.de>
2021-06-30 15:20:20 +0200

    add AllocInfo to API

--- src/app/api/nodes-api.service.ts
+++ src/app/api/nodes-api.service.ts
@@ -74,11 +74,22 @@ export class NodeInfo {
   diskInfo: DiskInfo;
   gpuInfo: GpuInfo[];
   buildInfo: BuildInfo;
+  allocInfo: AllocInfo[];
 
   // local only
   update: (node: NodeInfo) => void;
 
-  constructor(name: string, time: number, uptime: number, cpuInfo: CpuInfo, memInfo: MemInfo, gpus: GpuInfo[], buildInfo?: BuildInfo) {
+  constructor(
+    name: string,
+    time: number,
+    uptime: number,
+    cpuInfo: CpuInfo,
+    memInfo: MemInfo,
+    gpus: GpuInfo[],
+    buildInfo?: BuildInfo,
+    allocInfo?: AllocInfo[]
+  ) {
+
     this.name = name;
     this.time = time;
     this.uptime = uptime;
@@ -88,6 +99,7 @@ export class NodeInfo {
     this.diskInfo = new DiskInfo();
     this.gpuInfo = [];
     this.buildInfo = buildInfo == null ? new BuildInfo() : buildInfo;
+    this.allocInfo = allocInfo == null ? [] : allocInfo;
 
     for (const gpu of gpus) {
       this.gpuInfo.push(new GpuInfo(gpu.id, gpu.vendor, gpu.name));
@@ -167,3 +179,11 @@ export class NodeUtilization {
   gpuComputeUtilization: number[];
   gpuMemoryUtilization: number[];
 }
+
+export class AllocInfo {
+  title: string;
+  cpu: number;
+  memory: number;
+  gpu: number;
+}
+
