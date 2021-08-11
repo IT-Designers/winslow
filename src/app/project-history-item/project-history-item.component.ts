f9843334112f23ffb54357be70802d44beca4be2
Alexander Völker <alexander.voelker@it-designers.de>
2021-08-11 14:47:26 +0200

    add new project-history-item and project-history-item-details components

---
+++ src/app/project-history-item/project-history-item.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-project-history-item',
+  templateUrl: './project-history-item.component.html',
+  styleUrls: ['./project-history-item.component.css']
+})
+export class ProjectHistoryItemComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
