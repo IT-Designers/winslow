ec929b493b3c8bd3d346701ba4ffc17369e02e8c
Alexander Völker <alexander.voelker@it-designers.de>
2021-08-25 17:08:59 +0200

    implement new project history view (not finished yet)

--- src/app/project-history-item-details/project-history-item-details.component.ts
+++ src/app/project-history-item-details/project-history-item-details.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { ExecutionGroupInfo, StageInfo } from '../api/project-api.service';
 
 @Component({
   selector: 'app-project-history-item-details',
@@ -7,9 +8,63 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProjectHistoryItemDetailsComponent implements OnInit {
 
-  constructor() { }
+
+  @Input() entry: ExecutionGroupInfo;
+  @Input() entryNumber: number;
+  @Input() selectedStage: StageInfo;
+
+  @Input() firstEntry = true;
+  @Input() executionGroup: ExecutionGroupInfo;
+  @Input() expanded = false;
+  @Input() pipelineIsPaused: boolean = null;
+
+  @Output() clickResumeOnlyThisStage = new EventEmitter<ExecutionGroupInfo>();
+  @Output() clickResume = new EventEmitter<ExecutionGroupInfo>();
+  @Output() clickDelete = new EventEmitter<ExecutionGroupInfo>();
+  @Output() clickPauseAfterThis = new EventEmitter<ExecutionGroupInfo>();
+  @Output() clickKillStage = new EventEmitter<StageInfo>();
+  @Output() clickUseAsBlueprint = new EventEmitter<StageInfo>();
+  @Output() clickOpenLogs = new EventEmitter<StageInfo>();
+  @Output() clickOpenWorkspace = new EventEmitter<StageInfo>();
+  @Output() clickOpenTensorboard = new EventEmitter<StageInfo>();
+
+  buttonValue: string;
+  showStageDefinition: boolean;
+
+  constructor() {
+    this.buttonValue = "stage-definition";
+    this.showStageDefinition = true;
+
+  }
 
   ngOnInit(): void {
+
+  }
+
+  onButtonValueChange(value: string) {
+    this.buttonValue = value;
+  }
+
+  getStageDefinition() {
+    this.onButtonValueChange("stage-definition")
+    this.showStageDefinition = true;
+  }
+
+  getStageInformation() {
+    this.onButtonValueChange("stage-information")
+    this.showStageDefinition = false;
+  }
+
+  trackKey(keyValue: any): any {
+    return keyValue.key;
+  }
+
+  toDate(startTime: number): string {
+    if (startTime) {
+      return new Date(startTime).toLocaleString();
+    } else {
+      return '';
+    }
   }
 
 }
--- src/app/project-history-item/project-history-item.component.ts
+++ src/app/project-history-item/project-history-item.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { ExecutionGroupInfo, ProjectApiService, StageInfo, State } from '../api/project-api.service';
 
 @Component({
   selector: 'app-project-history-item',
@@ -7,9 +8,59 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProjectHistoryItemComponent implements OnInit {
 
-  constructor() { }
+  State = State;
+
+  @Input() firstEntry = true;
+  @Input() entryNumber = 0;
+  @Input() executionGroup: ExecutionGroupInfo;
+  @Input() expanded = false;
+  @Input() pipelineIsPaused: boolean = null;
+
+  constructor(private api: ProjectApiService) { }
 
   ngOnInit(): void {
+    console.log(this.firstEntry)
+    console.log(this.entryNumber)
+    console.log(this.executionGroup)
+    console.log(this.expanded)
+    console.log(this.pipelineIsPaused)
+  }
+
+  toDate(time: number) {
+    if (time) {
+      return new Date(time).toLocaleString();
+    } else {
+      return '';
+    }
+  }
+
+  trackStageInfo(value: StageInfo): string {
+    return value.id;
+  }
+
+  min(a: number, b: number) {
+    return Math.min(a, b);
+  }
+
+  max(a: number, b: number) {
+    return Math.max(a, b);
+  }
+
+  tryParseStageNumber(stageId: string, alt: number): number {
+    return this.api.tryParseStageNumber(stageId, alt);
+  }
+
+  getRangeEnvVariableValues(stage: StageInfo): string {
+    if (this.executionGroup.getGroupSize() > 1) {
+      return [...this.executionGroup
+        .rangedValues
+        .keys()]
+        .sort()
+        .map(e => e + '=' + stage.env.get(e))
+        .join(', ');
+    } else {
+      return null;
+    }
   }
 
 }
