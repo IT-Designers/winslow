945bf74e194fa431f90797ebeddccac4658b0533
Samuel Blickle <accounts@samuelblickle.de>
2021-09-14 11:13:36 +0200

    Add basic decision making for stage exectuion

--- application/src/test/java/de/itdesigners/winslow/fs/ObsoleteWorkspaceFinderTest.java
+++ application/src/test/java/de/itdesigners/winslow/fs/ObsoleteWorkspaceFinderTest.java
@@ -424,6 +424,7 @@ public class ObsoleteWorkspaceFinderTest {
                         null,
                         null,
                         null,
+                        null,
                         null
                 ),
                 null,
