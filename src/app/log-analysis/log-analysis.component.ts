8f5142f5d5dbc0c6ff5c402efae17abf599be5d2
Thorsten Rausch <thorsten.rausch@it-designers.de>
2022-01-17 16:34:59 +0100

    Get logs for analysis tab

--- src/app/log-analysis/log-analysis.component.ts
+++ src/app/log-analysis/log-analysis.component.ts
@@ -1,15 +1,68 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, Input, OnDestroy, OnInit} from '@angular/core';
+import {LogEntry, ProjectApiService, ProjectInfo} from "../api/project-api.service";
+import {Observable, Subscription} from "rxjs";
 
 @Component({
   selector: 'app-log-analysis',
   templateUrl: './log-analysis.component.html',
   styleUrls: ['./log-analysis.component.css']
 })
-export class LogAnalysisComponent implements OnInit {
+export class LogAnalysisComponent implements OnInit, OnDestroy {
 
-  constructor() { }
+
+  dummyLogObservable: Observable<LogEntry>;
+
+  logSubscription: Subscription = null;
+
+  logs?: LogEntry[] = [];
+
+  selectedProject: ProjectInfo = null;
+  selectedStageId: string = null;
+
+  @Input()
+  set project(value: ProjectInfo) {
+    this.selectedProject = value;
+  }
+
+  @Input()
+  set selectedStage(id: string) {
+    this.selectedStageId = id;
+  }
+
+  constructor(private api: ProjectApiService) {
+  }
 
   ngOnInit(): void {
+    this.subscribeLogs(this.selectedProject.id, this.selectedStageId)
+  }
+
+  ngOnDestroy() {
+    if (this.logSubscription) {
+      this.logSubscription.unsubscribe();
+      this.logSubscription = null;
+    }
+  }
+
+  private subscribeLogs(projectId: string, stageId = ProjectApiService.LOGS_LATEST) {
+    if (stageId == null) {
+      stageId = ProjectApiService.LOGS_LATEST;
+    }
+    //this.longLoading.raise(LogViewComponent.LONG_LOADING_FLAG);
+    this.logSubscription = this.api.watchLogs(projectId, (logs) => {
+      //this.longLoading.clear(LogViewComponent.LONG_LOADING_FLAG);
+      if (logs?.length > 0) {
+        this.logs.push(...logs);
+      } else {
+        this.logs = [];
+      }
+      console.log("LOGS:")
+      console.log(logs);
+      console.log(projectId, stageId);
+    }, stageId);
+  }
+
+  lineId(index, log): string {
+    return log.stageId + log.line;
   }
 
 }
