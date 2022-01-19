4bd3be44fecce0b4871f752ccb6e49041599bfa6
Thorsten Rausch <thorsten.rausch@it-designers.de>
2022-01-19 13:57:15 +0100

    Implement basic charts

---
+++ src/app/log-analysis-chart-dialog/log-analysis-chart-dialog.component.ts
@@ -0,0 +1,24 @@
+import {Component, Inject} from '@angular/core';
+import {MAT_DIALOG_DATA} from '@angular/material/dialog';
+import {LogEntry} from "../api/project-api.service";
+
+export interface LogChart {
+  name: string;
+  regExp: RegExp;
+  xAxisGroup: string;
+  yAxisGroup: string;
+}
+
+@Component({
+  selector: 'app-log-analysis-chart-dialog',
+  templateUrl: './log-analysis-chart-dialog.component.html',
+  styleUrls: ['./log-analysis-chart-dialog.component.css']
+})
+export class LogAnalysisChartDialogComponent {
+
+  constructor(
+    @Inject(MAT_DIALOG_DATA) public data: { chart: LogChart, logs: LogEntry[] }
+  ) {
+  }
+
+}
---
+++ src/app/log-analysis-chart/log-analysis-chart.component.ts
@@ -0,0 +1,78 @@
+import {Component, Input, OnInit} from '@angular/core';
+import {LogChart} from "../log-analysis-chart-dialog/log-analysis-chart-dialog.component";
+import {LogEntry, LogSource} from "../api/project-api.service";
+
+@Component({
+  selector: 'app-log-analysis-chart',
+  templateUrl: './log-analysis-chart.component.html',
+  styleUrls: ['./log-analysis-chart.component.css']
+})
+export class LogAnalysisChartComponent implements OnInit {
+
+  @Input() chart: LogChart;
+
+  @Input() logs: LogEntry[];
+
+  constructor() {
+  }
+
+  ngOnInit(): void {
+  }
+
+  getChartOptions(chart: LogChart, logs: LogEntry[]) {
+    // noinspection UnnecessaryLocalVariableJS
+    let chartOption = {
+      title: {
+        text: chart.name
+      },
+      grid: {
+        top: '50',
+        bottom: '20',
+        left: '40',
+        right: '10',
+      },
+      xAxis: {
+        type: 'value',
+        min: 0,
+        max: 'dataMax',
+      },
+      yAxis: {
+        type: 'value',
+        min: 0,
+        max: 'dataMax',
+      },
+      series: [
+        {
+          type: 'line',
+          data: this.getLogValuePairs(chart, logs)
+        }
+      ]
+    };
+
+    return chartOption
+  }
+
+  getLogValuePairs(chart: LogChart, logs: LogEntry[]) {
+    let results = [];
+    for (let log of logs) {
+      console.log(log.message)
+      if (log.source != LogSource.STANDARD_IO) continue;
+
+      let match = log.message.match(chart.regExp);
+      if (!match) continue;
+
+      let x = parseFloat(match.groups[chart.xAxisGroup]);
+      let y = parseFloat(match.groups[chart.yAxisGroup]);
+      console.log(x, y)
+      if (isNaN(x) || isNaN(y)) continue;
+
+      results.push([x, y]);
+    }
+    results.sort((pair1, pair2) => {
+      return pair1[0] - pair2[0]
+    })
+    console.log(results);
+    return results;
+  }
+
+}
