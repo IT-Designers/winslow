55179aad59ac27499ca092370f07cb90e9cb4205
Thorsten Rausch <thorsten.rausch@it-designers.de>
2022-01-24 11:50:49 +0100

    Add Regular Expression Editor component

---
+++ src/app/regular-expression-editor/regular-expression-editor.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-regular-expression-editor',
+  templateUrl: './regular-expression-editor.component.html',
+  styleUrls: ['./regular-expression-editor.component.css']
+})
+export class RegularExpressionEditorComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
