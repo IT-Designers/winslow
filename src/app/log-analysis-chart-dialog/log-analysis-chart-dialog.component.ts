df51519dbca938eeb623100b7d5970726e2b11f8
Thorsten Rausch <thorsten.rausch@it-designers.de>
2022-01-27 15:09:35 +0100

    Replace chart data model

--- src/app/log-analysis-chart-dialog/log-analysis-chart-dialog.component.ts
+++ src/app/log-analysis-chart-dialog/log-analysis-chart-dialog.component.ts
@@ -1,36 +1,7 @@
 import {Component, Inject} from '@angular/core';
 import {MAT_DIALOG_DATA} from '@angular/material/dialog';
 import {LogEntry, LogSource} from "../api/project-api.service";
-
-export enum ChartAxisType {
-  GROUP = "Capturing Group",
-  TIME = "Time",
-  STEPS = "Steps",
-}
-
-export class Chart {
-  name: string = "Unnamed chart";
-  regExpSource: string = 'ExampleLogEntry:.*x_value=(?<x>\\d+).*y_value=(?<y>\\d+)';
-
-  xAxisLabel: string = "x-Axis";
-  yAxisLabel: string = "y-Axis";
-  xAxisMinValue: string = "";
-  xAxisMaxValue: string = "";
-  yAxisMinValue: string = "0";
-  yAxisMaxValue: string = "10";
-
-  xAxisType: ChartAxisType = ChartAxisType.GROUP;
-  xAxisGroup: string = "x";
-
-  graphs: ChartGraph[] = [
-    new ChartGraph()
-  ];
-}
-
-export class ChartGraph {
-  label: string = "Unnamed graph";
-  yAxisGroup: string = "y";
-}
+import {ChartSettings, ChartAxisType} from "../log-analysis-chart/log-analysis-chart.component";
 
 @Component({
   selector: 'app-log-analysis-chart-dialog',
@@ -42,12 +13,8 @@ export class LogAnalysisChartDialogComponent {
   AxisTypes = Object.values(ChartAxisType);
 
   constructor(
-    @Inject(MAT_DIALOG_DATA) public data: { chart: Chart, logs: LogEntry[] }
+    @Inject(MAT_DIALOG_DATA) public data: { chart: ChartSettings, logs: LogEntry[] }
   ) {
-  }
-
-  addNewGraph() {
-    this.data.chart.graphs.push(new ChartGraph());
   }
 
   getPreviewText() {
