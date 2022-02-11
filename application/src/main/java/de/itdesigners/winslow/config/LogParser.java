30be94a312086462afe52f8b95bcee2a552dd0bd
Michael Watzko <michael.watzko@it-designers.de>
2022-02-11 10:51:36 +0100

    Revert "add initial charts definition support"
    This reverts commit 282c05c9b45746f40d9109978ce6092f6ed32dd4.
    
    We decided to implement this via front-end only changes and
    using the FilesApi to store JSON files on the backend.

--- application/src/main/java/de/itdesigners/winslow/config/LogParser.java
+++ application/src/main/java/de/itdesigners/winslow/config/LogParser.java
@@ -1,33 +1,23 @@
 package de.itdesigners.winslow.config;
 
 import javax.annotation.Nonnull;
-import javax.annotation.Nullable;
-import java.util.Collections;
-import java.util.List;
-import java.util.Optional;
 
 public class LogParser {
 
-    private final @Nonnull String      matcher;
-    private final @Nonnull String      destination;
-    private final @Nonnull String      formatter;
-    private final @Nonnull String      type;
-    private final @Nonnull List<Chart> charts;
+    private final @Nonnull String matcher;
+    private final @Nonnull String destination;
+    private final @Nonnull String formatter;
+    private final @Nonnull String type;
 
     public LogParser(
             @Nonnull String matcher,
             @Nonnull String destination,
             @Nonnull String formatter,
-            @Nonnull String type,
-            @Nullable List<Chart> charts) {
+            @Nonnull String type) {
         this.matcher     = matcher;
         this.destination = destination;
         this.formatter   = formatter;
         this.type        = type;
-        this.charts      = Optional
-                .ofNullable(charts)
-                .map(Collections::unmodifiableList)
-                .orElseGet(Collections::emptyList);
     }
 
     @Nonnull
@@ -48,10 +38,5 @@ public class LogParser {
     @Nonnull
     public String getType() {
         return type;
-    }
-
-    @Nonnull
-    public List<Chart> getCharts() {
-        return charts;
     }
 }
