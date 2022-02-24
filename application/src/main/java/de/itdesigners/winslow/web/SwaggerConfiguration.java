d7468ea50bc24bf78c986845fad2d2613cd8fa97
Stefan Kaufmann <stefan.kaufmann@it-designers.de>
2022-02-24 16:50:59 +0100

    upgrade swagger integration

--- application/src/main/java/de/itdesigners/winslow/web/SwaggerConfiguration.java
+++ application/src/main/java/de/itdesigners/winslow/web/SwaggerConfiguration.java
@@ -11,13 +11,11 @@ import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.service.ApiInfo;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
-import springfox.documentation.swagger2.annotations.EnableSwagger2WebMvc;
 
 import java.util.Optional;
 import java.util.stream.Stream;
 
 @Configuration
-@EnableSwagger2WebMvc
 @ComponentScan(basePackageClasses = WebApi.class)
 public class SwaggerConfiguration {
 
