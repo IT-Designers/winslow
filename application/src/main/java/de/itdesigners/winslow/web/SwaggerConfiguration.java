dddadfb23cbf300d58292e8406cc50d682dc964d
Stefan Kaufmann <stefan.kaufmann@it-designers.de>
2022-02-24 13:29:53 +0100

    spring version upgrade for java17

--- application/src/main/java/de/itdesigners/winslow/web/SwaggerConfiguration.java
+++ application/src/main/java/de/itdesigners/winslow/web/SwaggerConfiguration.java
@@ -11,13 +11,13 @@ import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.service.ApiInfo;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
-import springfox.documentation.swagger2.annotations.EnableSwagger2;
+import springfox.documentation.swagger2.annotations.EnableSwagger2WebMvc;
 
 import java.util.Optional;
 import java.util.stream.Stream;
 
 @Configuration
-@EnableSwagger2
+@EnableSwagger2WebMvc
 @ComponentScan(basePackageClasses = WebApi.class)
 public class SwaggerConfiguration {
 
