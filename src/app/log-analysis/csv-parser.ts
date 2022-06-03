3607957c013ec6126fb60f9be790270a1f9f2a45
Thorsten Rausch <thorsten.rausch@it-designers.de>
2022-06-03 16:58:17 +0200

    cleanup code

--- src/app/log-analysis/csv-parser.ts
+++ src/app/log-analysis/csv-parser.ts
@@ -6,16 +6,15 @@ export type CsvParserOptions = {
   quotationMarks: string[]
 }
 
+// tokens are searched for in given order
 const DEFAULT_OPTIONS: CsvParserOptions = {
-  rowDelimiters: ['\r\n', '\r', '\n'],
+  rowDelimiters: ['\r\n', '\n\r', '\r', '\n'],
   colDelimiters: [',', ';', '\t'],
   quotationMarks: ['"'],
 }
 
 export function parseCsv(text: string, formatOptions = DEFAULT_OPTIONS): CsvFileContent {
   const textLength = text.length
-  const {rowDelimiters, colDelimiters, quotationMarks} = formatOptions
-  const allTokens = [...rowDelimiters, ...colDelimiters, ...quotationMarks]
 
   let parsed: CsvFileContent = []
 
@@ -24,41 +23,42 @@ export function parseCsv(text: string, formatOptions = DEFAULT_OPTIONS): CsvFile
   let index = 0;
 
   while (index < textLength) {
-    const nextToken = findToken(text, index, allTokens)
+    const nextToken = findToken(text, index, formatOptions)
+    switch (nextToken.type) {
 
-    // end of file
-    if (nextToken.index == -1) {
-      if (expectFieldContent) nextRow.push(text.substring(index, textLength))
-      expectFieldContent = true
-      parsed.push(nextRow)
-      nextRow = []
-      index = textLength
-    }
+      // end of file
+      case TokenType.NONE_FOUND:
+        if (expectFieldContent) nextRow.push(text.substring(index, textLength))
+        expectFieldContent = true
+        parsed.push(nextRow)
+        nextRow = []
+        index = textLength
+        break;
 
-    // end of line
-    else if (rowDelimiters.includes(nextToken.literal)) {
-      if (expectFieldContent) nextRow.push(text.substring(index, nextToken.index))
-      expectFieldContent = true
-      parsed.push(nextRow)
-      nextRow = []
-      index = nextToken.index + nextToken.literal.length
-    }
+      // end of line
+      case TokenType.ROW_DELIMITER:
+        if (expectFieldContent) nextRow.push(text.substring(index, nextToken.index))
+        expectFieldContent = true
+        parsed.push(nextRow)
+        nextRow = []
+        index = nextToken.index + nextToken.literal.length
+        break;
 
-    // end of field
-    else if (colDelimiters.includes(nextToken.literal)) {
-      if (expectFieldContent) nextRow.push(text.substring(index, nextToken.index))
-      expectFieldContent = true
-      index = nextToken.index + nextToken.literal.length
-    }
+      // end of field
+      case TokenType.COL_DELIMITER:
+        if (expectFieldContent) nextRow.push(text.substring(index, nextToken.index))
+        expectFieldContent = true
+        index = nextToken.index + nextToken.literal.length
+        break;
 
-    // start of quote
-    else if (quotationMarks.includes(nextToken.literal)) {
-      const quoteStartIndex = nextToken.index + nextToken.literal.length
-      const quoteEndIndex = text.indexOf(nextToken.literal, quoteStartIndex + nextToken.literal.length)
-      nextRow.push(text.substring(quoteStartIndex, quoteEndIndex))
-      expectFieldContent = false
-      index = quoteEndIndex + nextToken.literal.length
-      //todo escape
+      // start of quote
+      case TokenType.QUOTATION_MARK:
+        const quoteStartIndex = nextToken.index + nextToken.literal.length
+        const quoteEndIndex = text.indexOf(nextToken.literal, quoteStartIndex + nextToken.literal.length)
+        nextRow.push(text.substring(quoteStartIndex, quoteEndIndex))
+        expectFieldContent = false
+        index = quoteEndIndex + nextToken.literal.length
+        break;
     }
   }
 
@@ -68,14 +68,44 @@ export function parseCsv(text: string, formatOptions = DEFAULT_OPTIONS): CsvFile
   return parsed
 }
 
+enum TokenType {
+  NONE_FOUND,
+  ROW_DELIMITER,
+  COL_DELIMITER,
+  QUOTATION_MARK,
+}
+
 interface SearchResult {
   index: number
   literal: string
+  type: TokenType
 }
 
-function findToken(source: string, start: number, tokens: string[]): SearchResult {
-  return tokens
-    .map(token => ({index: source.indexOf(token, start), literal: token}))
-    .filter(result => result.index != -1)
-    .reduce((prev, current) => prev.index < current.index ? prev : current)
+function findToken(source: string, start: number, options: CsvParserOptions): SearchResult {
+  try {
+    return ([
+      ...options.rowDelimiters.map(token => ({
+        index: source.indexOf(token, start),
+        literal: token,
+        type: TokenType.ROW_DELIMITER
+      })),
+      ...options.colDelimiters.map(token => ({
+        index: source.indexOf(token, start),
+        literal: token,
+        type: TokenType.COL_DELIMITER
+      })),
+      ...options.quotationMarks.map(token => ({
+        index: source.indexOf(token, start),
+        literal: token,
+        type: TokenType.QUOTATION_MARK
+      })),
+    ]).filter(result => result.index != -1)
+      .reduce((prev, current) => prev.index < current.index ? prev : current)
+  } catch (error) {
+    return {
+      index: -1,
+      literal: "",
+      type: TokenType.NONE_FOUND,
+    }
+  }
 }
