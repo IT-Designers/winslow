c0434d22c10ba2e04d09e0394a7698f86bc7a1e0
Thorsten Rausch <thorsten.rausch@it-designers.de>
2022-06-17 13:36:07 +0200

    add csv filename and directory to CvsFileInfo

--- src/app/log-analysis/csv-file-controller.ts
+++ src/app/log-analysis/csv-file-controller.ts
@@ -1,8 +1,15 @@
 import {FilesApiService} from "../api/files-api.service";
 import {BehaviorSubject, combineLatest, Observable, Subject} from "rxjs";
 import {CsvFileContent, parseCsv} from "./csv-parser";
-import {switchMap} from "rxjs/operators";
+import {map, switchMap} from "rxjs/operators";
 import {StageInfo} from "../api/project-api.service";
+
+export interface CsvFileInfo {
+  stageId: string
+  filename: string
+  directory: string
+  content: CsvFileContent
+}
 
 export class CsvFileController {
   static readonly PATH_TO_WORKSPACES = '/workspaces';
@@ -16,19 +23,32 @@ export class CsvFileController {
     this.stages$ = new BehaviorSubject<StageCsvInfo[]>([]);
   }
 
-  getCsvContents$(filename: string): Observable<CsvFileContent[]> {
+  getCsvFiles$(filename: string): Observable<CsvFileInfo[]> {
     return this.stages$.pipe(
       switchMap(stages => {
-        const content$s = stages.map(stage => {
-          const csvFileInfo = this.selectCsvFile(stage, filename);
-          return csvFileInfo.content$;
-        })
-        return combineLatest(content$s);
+        const info$s = stages.map(stage => this.getFileInfo$(stage, filename))
+        return combineLatest(info$s);
       }),
     )
   }
 
-  private selectCsvFile(stage: StageCsvInfo, filename: string): CsvFileSource {
+  private static getFileDirectory(stageCsvInfo: StageCsvInfo) {
+    return `${CsvFileController.PATH_TO_WORKSPACES}/${stageCsvInfo.stage.workspace}/.log_parser_output`;
+  }
+
+  private getFileInfo$(stage: StageCsvInfo, filename: string): Observable<CsvFileInfo> {
+    const csvFileSource = this.getCsvFileSource(stage, filename);
+    return csvFileSource.content$.pipe(
+      map((content: CsvFileContent): CsvFileInfo => ({
+        content: content,
+        directory: csvFileSource.directory,
+        filename: csvFileSource.filename,
+        stageId: stage.id
+      }))
+    )
+  }
+
+  private getCsvFileSource(stage: StageCsvInfo, filename: string): CsvFileSource {
     let csvFileSource = stage.csvFiles.find(csvFile => csvFile.filename == filename)
     if (csvFileSource == null) {
       csvFileSource = this.loadCsvFile(stage, filename);
@@ -66,10 +86,6 @@ export class CsvFileController {
       });
 
     return csvFileSource;
-  }
-
-  private static getFileDirectory(stageCsvInfo: StageCsvInfo) {
-    return `${CsvFileController.PATH_TO_WORKSPACES}/${stageCsvInfo.stage.workspace}/.log_parser_output`;
   }
 }
 
