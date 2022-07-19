bc9bb99b11faf7bbf20bbf89fa6350309784c2cd
Thorsten Rausch <thorsten.rausch@it-designers.de>
2022-07-19 09:47:01 +0200

    unsubscribe from charts

--- src/app/log-analysis-chart-dialog/log-analysis-chart-dialog.component.ts
+++ src/app/log-analysis-chart-dialog/log-analysis-chart-dialog.component.ts
@@ -1,4 +1,4 @@
-import {Component, Inject} from '@angular/core';
+import {Component, Inject, OnDestroy} from '@angular/core';
 import {MAT_DIALOG_DATA} from '@angular/material/dialog';
 import {
   ChartAxisType,
@@ -9,31 +9,33 @@ import {
   LogChartSnapshot
 } from "../log-analysis/log-chart-definition";
 import {CsvFile} from "../log-analysis/csv-file-controller";
+import {Subscription} from "rxjs";
 
 @Component({
   selector: 'app-log-analysis-chart-dialog',
   templateUrl: './log-analysis-chart-dialog.component.html',
   styleUrls: ['./log-analysis-chart-dialog.component.css']
 })
-export class LogAnalysisChartDialogComponent {
+export class LogAnalysisChartDialogComponent implements OnDestroy {
 
   AxisTypes = Object.values(ChartAxisType);
   chart: LogChart;
   definition: LogChartDefinition;
   latestSnapshot: LogChartSnapshot;
+  subscription: Subscription;
 
   constructor(@Inject(MAT_DIALOG_DATA) private data: ChartDialogData) {
     this.chart = new LogChart(data.csvFileController, data.chart.filename, data.definition);
 
     this.definition = Object.assign(new LogChartDefinition(), data.definition);
     this.definition.displaySettings = Object.assign(new ChartDisplaySettings(), data.definition.displaySettings);
-
-    this.chart.snapshot$.subscribe(snapshot => {
-      console.log()
-      return this.latestSnapshot = snapshot;
-    })
+    this.subscription = this.chart.snapshot$.subscribe(snapshot => this.latestSnapshot = snapshot)
 
     this.refresh()
+  }
+
+  ngOnDestroy() {
+    this.subscription?.unsubscribe()
   }
 
   refresh() {
---
+++ src/app/log-analysis-chart/log-analysis-chart.component.ts
@@ -0,0 +1,80 @@
+import {Component, Input, OnDestroy, OnInit} from '@angular/core';
+import {ChartDataSet, ChartDisplaySettings, LogChart} from "../log-analysis/log-chart-definition";
+import {Subscription} from "rxjs";
+
+@Component({
+  selector: 'app-log-analysis-chart',
+  templateUrl: './log-analysis-chart.component.html',
+  styleUrls: ['./log-analysis-chart.component.css']
+})
+export class LogAnalysisChartComponent implements OnInit, OnDestroy {
+
+  options: {}
+  subscription: Subscription;
+
+  @Input() chart: LogChart
+
+  ngOnInit() {
+    this.subscription = this.chart.snapshot$.subscribe({
+      next: snapshot => {
+        this.settings(snapshot.definition.displaySettings)
+        this.data(snapshot.chartData)
+      }
+    })
+  }
+
+  ngOnDestroy() {
+    this.subscription?.unsubscribe()
+  }
+
+  settings(settings: ChartDisplaySettings) {
+    const newOptions = {
+      title: {
+        text: settings.name,
+      },
+      grid: {
+        top: 50,
+        bottom: 35,
+        left: 40,
+        right: 10,
+      },
+      xAxis: {
+        name: settings.xAxisName,
+        type: settings.xAxisType,
+        min: settings.xAxisMinValue ?? 'dataMin',
+        max: settings.xAxisMaxValue ?? 'dataMax',
+        nameLocation: 'center',
+        nameGap: 25,
+      },
+      yAxis: {
+        name: settings.yAxisName,
+        type: settings.yAxisType,
+        min: settings.yAxisMinValue ?? 'dataMin',
+        max: settings.yAxisMaxValue ?? 'dataMax',
+        nameLocation: 'center',
+        nameGap: 25,
+      },
+      animation: false,
+    }
+    this.updateOptions(newOptions);
+  };
+
+  data(chartData: ChartDataSet[]) {
+    const series = chartData.map(data => ({
+      type: 'line',
+      showSymbol: false,
+      data: data,
+    }))
+    const newOptions = {
+      series: series,
+    }
+    this.updateOptions(newOptions);
+  }
+
+  constructor() {
+  }
+
+  private updateOptions(newOptions) {
+    this.options = {...this.options, ...newOptions}
+  }
+}
