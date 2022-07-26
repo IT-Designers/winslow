e6f7681876864d80fd3f9a480bb3665b95c0fedc
Michael Watzko <michael.watzko@it-designers.de>
2022-07-26 10:51:45 +0200

    Replace RangedValueJsonModule with annotation on RangedValues in the API

--- api/src/main/java/de/itdesigners/winslow/api/pipeline/RangedValue.java
+++ api/src/main/java/de/itdesigners/winslow/api/pipeline/RangedValue.java
@@ -1,5 +1,16 @@
 package de.itdesigners.winslow.api.pipeline;
 
+import com.fasterxml.jackson.annotation.JsonSubTypes;
+import com.fasterxml.jackson.annotation.JsonTypeInfo;
+
+@JsonTypeInfo(
+        use = JsonTypeInfo.Id.NAME,
+        include = JsonTypeInfo.As.WRAPPER_OBJECT
+)
+@JsonSubTypes({
+        @JsonSubTypes.Type(value = RangeWithStepSize.class, name = "DiscreteSteps"),
+        @JsonSubTypes.Type(value = RangedList.class, name = "List")
+})
 public interface RangedValue {
 
     String getValue(int step);
