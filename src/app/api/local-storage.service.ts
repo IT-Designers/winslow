1743c757bdc8d422a56196141d968a831665c2c7
Eric Höfert <eric.hoefert@it-designers.de>
2022-08-05 15:27:20 +0200

    add functions to get, set in local storage with a key

--- src/app/api/local-storage.service.ts
+++ src/app/api/local-storage.service.ts
@@ -8,28 +8,36 @@ export class LocalStorageService {
   constructor() {
   }
 
-  private readonly KEY_CHART_SETTINGS = "winslow-chart-settings"
+  private readonly KEY_CHART_SETTINGS = 'winslow-chart-settings';
 
   getChartSettings() {
-    const item = JSON.parse(localStorage.getItem(this.KEY_CHART_SETTINGS))
-    return item ?? new GlobalChartSettings()
+    const item = JSON.parse(localStorage.getItem(this.KEY_CHART_SETTINGS));
+    return item ?? new GlobalChartSettings();
   }
 
   setChartSettings(item: GlobalChartSettings) {
-    localStorage.setItem(this.KEY_CHART_SETTINGS, JSON.stringify(item))
+    localStorage.setItem(this.KEY_CHART_SETTINGS, JSON.stringify(item));
+  }
+
+  getSettings(KEY: string) {
+    return JSON.parse(localStorage.getItem(KEY));
+  }
+
+  setSettings(KEY: string, data: any) {
+    localStorage.setItem(KEY, JSON.stringify(data));
   }
 }
 
 export class GlobalChartSettings {
   constructor() {
-    this.enableEntryLimit = false
-    this.entryLimit = 50
-    this.enableRefreshing = true
-    this.refreshTimerInSeconds = 5
+    this.enableEntryLimit = false;
+    this.entryLimit = 50;
+    this.enableRefreshing = true;
+    this.refreshTimerInSeconds = 5;
   }
 
-  enableEntryLimit: boolean
-  entryLimit: number
-  enableRefreshing: boolean
-  refreshTimerInSeconds: number
+  enableEntryLimit: boolean;
+  entryLimit: number;
+  enableRefreshing: boolean;
+  refreshTimerInSeconds: number;
 }
