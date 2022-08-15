23657953d8d2b3e17e7fca3c53235a2ea290266b
Thorsten Rausch <thorsten.rausch@it-designers.de>
2022-08-15 17:09:29 +0200

    fix incorrect filename in csv-files.service.spec.ts

--- src/app/log-analysis/csv-files.service.spec.ts
+++ src/app/log-analysis/csv-files.service.spec.ts
@@ -2,7 +2,7 @@ import { TestBed } from '@angular/core/testing';
 
 import { CsvFilesService } from './csv-files.service';
 
-describe('LogAnalysisFileService', () => {
+describe('CsvFilesService', () => {
   let service: CsvFilesService;
 
   beforeEach(() => {
