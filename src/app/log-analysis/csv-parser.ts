4fef9d9978d1f3b1b3eb325cf87ef773c3e0962e
Thorsten Rausch <thorsten.rausch@it-designers.de>
2022-09-08 12:28:26 +0200

    replace csv-parser with more readable version

--- src/app/log-analysis/csv-parser.ts
+++ src/app/log-analysis/csv-parser.ts
@@ -11,127 +11,145 @@ const DEFAULT_OPTIONS: CsvParserOptions = {
   rowDelimiters: ['\r\n', '\n\r', '\r', '\n'],
   colDelimiters: [',', ';', '\t'],
   quotationMarks: ['"'],
-}
-
-export function parseCsv(text: string, formatOptions = DEFAULT_OPTIONS): CsvFileContent {
-  const textLength = text.length
-
-  let parsed: CsvFileContent = []
-
-  let nextRow: string[] = []
-  let startOfNewField = true
-  let index = 0;
-
-  while (index < textLength) {
-    const nextToken = findToken(text, index, formatOptions)
-    switch (nextToken.type) {
-
-      // end of file
-      case TokenType.NONE_FOUND:
-        if (startOfNewField) nextRow.push(text.substring(index, textLength))
-        startOfNewField = true
-        parsed.push(nextRow)
-        nextRow = []
-        index = textLength
-        break;
-
-      // end of line
-      case TokenType.ROW_DELIMITER:
-        if (startOfNewField) nextRow.push(text.substring(index, nextToken.index))
-        startOfNewField = true
-        parsed.push(nextRow)
-        nextRow = []
-        index = nextToken.index + nextToken.literal.length
-        break;
-
-      // end of field
-      case TokenType.COL_DELIMITER:
-        if (startOfNewField) nextRow.push(text.substring(index, nextToken.index))
-        startOfNewField = true
-        index = nextToken.index + nextToken.literal.length
-        break;
-
-      // start of quote
-      case TokenType.QUOTATION_MARK:
-        const quoteStartIndex = nextToken.index + nextToken.literal.length
-        const quotedFieldInfo = parseQuotedField(text, quoteStartIndex, nextToken.literal)
-        nextRow.push(quotedFieldInfo.content)
-        startOfNewField = false
-        index = quotedFieldInfo.endIndex
-        break;
-    }
-  }
-
-  return parsed
-}
-
-function parseQuotedField(text: string, startIndex: number, quotationMark: string) {
-  const escapeSequence = `${quotationMark}${quotationMark}`
-  let quoteContent = ""
-  let index = startIndex
-
-  while (true) {
-    const nextQuotationMark = text.indexOf(quotationMark, startIndex)
-    const nextEscapeSequence = text.indexOf(escapeSequence, startIndex)
-    if (nextQuotationMark == -1) {
-      console.warn("Unresolved quote in csv file.")
-      quoteContent += text.substring(index)
-      index = text.length
-      break;
-    } else if (nextEscapeSequence == -1 || nextQuotationMark < nextEscapeSequence) {
-      quoteContent += text.substring(index, nextQuotationMark)
-      index = nextQuotationMark + quotationMark.length
-      break;
-    } else {
-      quoteContent += text.substring(index, nextEscapeSequence) + quotationMark
-      index = nextEscapeSequence + escapeSequence.length
-    }
-  }
-
-  return {
-    endIndex: index,
-    content: quoteContent
-  }
-}
+};
 
 enum TokenType {
-  NONE_FOUND,
-  ROW_DELIMITER,
-  COL_DELIMITER,
-  QUOTATION_MARK,
+  NoToken,
+  ColDelimiter,
+  RowDelimiter,
+  QuotationMark,
 }
 
-interface SearchResult {
-  index: number
-  literal: string
+type TokenInfo = {
+  literal: string,
   type: TokenType
 }
 
-function findToken(text: string, start: number, options: CsvParserOptions): SearchResult {
-  try {
-    return ([
-      ...options.rowDelimiters.map(token => ({
-        index: text.indexOf(token, start),
-        literal: token,
-        type: TokenType.ROW_DELIMITER
-      })),
-      ...options.colDelimiters.map(token => ({
-        index: text.indexOf(token, start),
-        literal: token,
-        type: TokenType.COL_DELIMITER
-      })),
-      ...options.quotationMarks.map(token => ({
-        index: text.indexOf(token, start),
-        literal: token,
-        type: TokenType.QUOTATION_MARK
-      })),
-    ]).filter(result => result.index != -1)
-      .reduce((prev, current) => prev.index < current.index ? prev : current)
-  } catch (error) {
-    return {
-      index: -1,
-      literal: "",
-      type: TokenType.NONE_FOUND,
-    }
+
+enum State {
+  BeforeQuote,
+  InsideQuote,
+  BeyondQuote,
+}
+
+function toTokenList(options: CsvParserOptions): TokenInfo[] {
+  return [
+    ...options.rowDelimiters.map(token => ({
+      literal: token,
+      type: TokenType.RowDelimiter
+    })),
+    ...options.colDelimiters.map(token => ({
+      literal: token,
+      type: TokenType.ColDelimiter
+    })),
+    ...options.quotationMarks.map(token => ({
+      literal: token,
+      type: TokenType.QuotationMark
+    })),
+  ];
+}
+
+export function parseCsv(text: string, options: CsvParserOptions = DEFAULT_OPTIONS) {
+  const tokens: TokenInfo[] = toTokenList(options);
+
+  let fileContent = [];
+  let lineContent = [];
+  let fieldContent = '';
+
+  function resetField() {
+    fieldContent = '';
   }
+
+  function pushCharsToField(literal: string) {
+    fieldContent = fieldContent.concat(literal);
+  }
+
+  function pushFieldToLine() {
+    lineContent.push(fieldContent);
+    fieldContent = '';
+  }
+
+  function pushLineToFile() {
+    fileContent.push(lineContent);
+    lineContent = [];
+  }
+
+  let parserState: State = State.BeforeQuote;
+  let position: number = 0;
+
+  while (position < text.length) {
+
+    // check if there is a token at the current position
+    let increment: number = 1;
+    let tokenType: TokenType = TokenType.NoToken;
+    let tokenLiteral: string = null;
+
+    for (const token of tokens) {
+      const tokenLength = token.literal.length;
+      if (text.slice(position, position + tokenLength) == token.literal) {
+        tokenType = token.type;
+        tokenLiteral = token.literal;
+        increment = tokenLength;
+        break;
+      }
+    }
+
+    // handle or ignore the token depending on quotes
+    switch (parserState) {
+      case State.BeforeQuote:
+        switch (tokenType) {
+          case TokenType.NoToken:
+            pushCharsToField(text.charAt(position));
+            break;
+          case TokenType.ColDelimiter:
+            pushFieldToLine();
+            break;
+          case TokenType.RowDelimiter:
+            pushLineToFile();
+            break;
+          case TokenType.QuotationMark:
+            resetField();
+            parserState = State.InsideQuote;
+            break;
+        }
+        break;
+
+      case State.InsideQuote:
+        switch (tokenType) {
+          case TokenType.NoToken:
+          case TokenType.ColDelimiter:
+          case TokenType.RowDelimiter:
+            pushCharsToField(text.charAt(position));
+            break;
+          case TokenType.QuotationMark:
+            parserState = State.BeyondQuote;
+            break;
+        }
+        break;
+
+      case State.BeyondQuote:
+        switch (tokenType) {
+          case TokenType.NoToken:
+            break;
+          case TokenType.ColDelimiter:
+            pushFieldToLine();
+            parserState = State.BeforeQuote;
+            break;
+          case TokenType.RowDelimiter:
+            pushLineToFile();
+            parserState = State.BeforeQuote;
+            break;
+          case TokenType.QuotationMark:
+            pushCharsToField(tokenLiteral);
+            parserState = State.InsideQuote;
+            break;
+        }
+        break;
+    }
+    position += increment;
+  }
+
+  pushFieldToLine();
+  pushLineToFile();
+  return fileContent;
 }
