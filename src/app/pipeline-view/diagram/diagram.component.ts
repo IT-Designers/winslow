75acee9ecb141f13c338803b909dac21844b45c9
ri4ho7 <rico.hofmann@it-designers.de>
2022-09-20 11:12:03 +0200

    Bpmn working with properties panel

--- src/app/pipeline-view/diagram/diagram.component.ts
+++ src/app/pipeline-view/diagram/diagram.component.ts
@@ -1,116 +1,62 @@
 import {
-  AfterContentInit,
+  OnInit,
   Component,
-  ElementRef,
-  Input,
-  OnChanges,
-  OnDestroy,
-  Output,
-  ViewChild,
-  SimpleChanges,
-  EventEmitter
+  Input
 } from '@angular/core';
 
-import { HttpClient } from '@angular/common/http';
-import { map, switchMap } from 'rxjs/operators';
+import {HttpClient} from '@angular/common/http';
 
-/**
- * You may include a different variant of BpmnJS:
- *
- * bpmn-viewer  - displays BPMN diagrams without the ability
- *                to navigate them
- * bpmn-modeler - bootstraps a full-fledged BPMN editor
- */
+import BpmnModeler from 'bpmn-js/lib/Modeler.js';
 import * as BpmnJS from 'bpmn-js/dist/bpmn-modeler.production.min.js';
+import {BpmnPropertiesPanelModule, BpmnPropertiesProviderModule} from 'bpmn-js-properties-panel';
 
-import { from, Observable, Subscription } from 'rxjs';
-import {PipelineApiService, PipelineInfo} from "../../api/pipeline-api.service";
+import {Observable} from 'rxjs';
+import {ProjectInfo} from "../../api/project-api.service";
 
 @Component({
   selector: 'app-diagram',
-  template: `
-    <div #ref class="diagram-container"></div>
-  `,
-  styles: [
-    `
-      .diagram-container {
-        height: 100%;
-        width: 100%;
-      }
-    `
-  ]
+  templateUrl: './diagram.component.html',
+  styleUrls: ['./diagram.component.css']
 })
-export class DiagramComponent implements AfterContentInit, OnChanges, OnDestroy {
-private bpmnJS: BpmnJS;
+export class DiagramComponent implements OnInit {
+  modeler: BpmnModeler;
+  @Input() projectDef: ProjectInfo;
 
-@ViewChild('ref', { static: true }) private el: ElementRef;
-@Output() private importDone: EventEmitter<any> = new EventEmitter();
+  constructor(private http: HttpClient) {
+  }
 
-@Input() private url: string;
-//@Input() private pipeline: PipelineInfo;
-
-  constructor(
-    private api: PipelineApiService,
-    private http: HttpClient) {
-
-    this.bpmnJS = new BpmnJS();
-
-    this.bpmnJS.on('import.done', ({ error }) => {
-      if (!error) {
-        this.bpmnJS.get('canvas').zoom('fit-viewport');
-      }
+  ngOnInit(): void {
+    this.modeler = new BpmnModeler({
+      container: '#js-canvas',
+      width: '100%',
+      height: '100%',
+      propertiesPanel: {
+        parent: '#js-properties-panel'
+      },
+      additionalModules: [
+        BpmnPropertiesPanelModule,
+        BpmnPropertiesProviderModule
+      ],
     });
-
+    this.load();
   }
 
-  ngAfterContentInit(): void {
-    this.bpmnJS.attachTo(this.el.nativeElement);
+  load(): void {
+    console.dir(this.projectDef.pipelineDefinition);
+    this.getExample().subscribe(data => {
+      this.modeler.importXML(data, value => this.handleError(value));
+      console.log(this.modeler.get('moddle'));
+    });
   }
 
-  ngOnChanges(changes: SimpleChanges) {
-    // re-import whenever the url changes
-    if (changes.url) {
-      this.loadUrl(changes.url.currentValue);
+  handleError(err: any) {
+    if (err) {
+      console.warn('XML Import Error:' + err);
     }
   }
 
-  ngOnDestroy(): void {
-    this.bpmnJS.destroy();
-  }
-
-  /**
-   * Load diagram from URL and emit completion event
-   */
-  loadUrl(url: string): Subscription {
-
-    return (
-      this.http.get(url, { responseType: 'text' }).pipe(
-        switchMap((xml: string) => this.importDiagram(xml)),
-        map(result => result.warnings),
-      ).subscribe(
-        (warnings) => {
-          this.importDone.emit({
-            type: 'success',
-            warnings
-          });
-        },
-        (err) => {
-          this.importDone.emit({
-            type: 'error',
-            error: err
-          });
-        }
-      )
-    );
-  }
-
-  /**
-   * Creates a Promise to import the given XML into the current
-   * BpmnJS instance, then returns it as an Observable.
-   *
-   * @see https://github.com/bpmn-io/bpmn-js-callbacks-to-promises#importxml
-   */
-private importDiagram(xml: string): Observable<{warnings: Array<any>}> {
-    return from(this.bpmnJS.importXML(xml) as Promise<{warnings: Array<any>}>);
+  public getExample(): Observable<string> {
+    const url = 'https://cdn.staticaly.com/gh/bpmn-io/bpmn-js-examples/dfceecba/starter/diagram.bpmn';
+    return this.http.get(url, {responseType: 'text'});
   }
 }
