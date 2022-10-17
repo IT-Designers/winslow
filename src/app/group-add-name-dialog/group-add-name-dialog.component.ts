2d0c539008a423bdc24eef3aedd04a0e8787f5c5
ky4me6 <kyle.mezger@it-designers.de>
2022-10-17 15:22:48 +0200

    forgot to add unversioned files to previous commits

---
+++ src/app/group-add-name-dialog/group-add-name-dialog.component.ts
@@ -0,0 +1,30 @@
+import {Component, Inject, OnInit} from '@angular/core';
+import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material/dialog';
+
+/*export interface GroupNameDialogData {
+  name: string;
+}*/
+
+
+@Component({
+  selector: 'app-group-add-name-dialog',
+  templateUrl: './group-add-name-dialog.component.html',
+  styleUrls: ['./group-add-name-dialog.component.css']
+})
+export class GroupAddNameDialogComponent implements OnInit {
+
+  nameInput: string;
+
+  constructor(
+    public dialogRef: MatDialogRef<GroupAddNameDialogComponent>,
+    @Inject(MAT_DIALOG_DATA) public data: string
+  ) { }
+
+  ngOnInit(): void {
+  }
+
+  /*updateData() {
+    this.data.name = this.nameInput;
+  }*/
+
+}
