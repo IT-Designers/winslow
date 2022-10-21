31978eb63d4926ab25d237a46c37f95524ffdb98
Michael Watzko <michael.watzko@it-designers.de>
2022-10-21 18:22:29 +0200

    Add helper methods to Link for creating member and owner Links

--- api/src/main/java/de/itdesigners/winslow/api/auth/Link.java
+++ api/src/main/java/de/itdesigners/winslow/api/auth/Link.java
@@ -2,4 +2,27 @@ package de.itdesigners.winslow.api.auth;
 
 import javax.annotation.Nonnull;
 
-public record Link(@Nonnull String name, @Nonnull Role role) {}
+public record Link(@Nonnull String name, @Nonnull Role role) {
+
+
+    /**
+     * Helper that creates {@link Link} with {@link Role#MEMBER} for the given name
+     * @param name The name to create the link for
+     * @return The new {@link Link} with the given name and {@link Role#MEMBER}
+     */
+    @Nonnull
+    public static Link member(@Nonnull String name) {
+        return new Link(name, Role.MEMBER);
+    }
+
+    /**
+     * Helper that creates {@link Link} with {@link Role#OWNER} for the given name
+     * @param name The name to create the link for
+     * @return The new {@link Link} with the given name and {@link Role#OWNER}
+     */
+    @Nonnull
+    public static Link owner(@Nonnull String name) {
+        return new Link(name, Role.OWNER);
+    }
+
+}
