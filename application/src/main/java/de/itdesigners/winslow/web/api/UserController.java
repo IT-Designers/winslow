ce6b0bc1c5444a3c6825682f02e15746c2395e36
Michael Watzko <michael.watzko@it-designers.de>
2022-11-02 14:35:40 +0100

    Add listing of users and a specific user to UserController

--- application/src/main/java/de/itdesigners/winslow/web/api/UserController.java
+++ application/src/main/java/de/itdesigners/winslow/web/api/UserController.java
@@ -2,14 +2,19 @@ package de.itdesigners.winslow.web.api;
 
 import de.itdesigners.winslow.Winslow;
 import de.itdesigners.winslow.api.auth.UserInfo;
+import de.itdesigners.winslow.auth.InvalidNameException;
 import de.itdesigners.winslow.auth.User;
 import de.itdesigners.winslow.web.UserInfoConverter;
 import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.annotation.Nonnull;
+import javax.annotation.Nullable;
 import java.util.Optional;
 import java.util.logging.Logger;
 import java.util.stream.Stream;
@@ -39,5 +44,43 @@ public class UserController {
                 .getUserManager()
                 .getUsersPotentiallyIncomplete()
                 .map(UserInfoConverter::from);
+    }
+
+    @GetMapping("/users/{name}/available")
+    public ResponseEntity<String> getUserNameAvailable(
+            @Nonnull User user,
+            @PathVariable("name") String name) {
+        try {
+            InvalidNameException.ensureValid(name);
+            if (winslow
+                    .getUserManager()
+                    .getUser(name)
+                    .isPresent()) {
+                return new ResponseEntity<>("Name already taken.", HttpStatus.BAD_REQUEST);
+            }
+            return new ResponseEntity<>(HttpStatus.OK);
+        } catch (InvalidNameException e) {
+            return new ResponseEntity<>(
+                    "Invalid name. Name must contain only: " + InvalidNameException.REGEX_PATTERN_DESCRIPTION + " and not exceed "+ InvalidNameException.MAX_LENGTH +" characters",
+                    HttpStatus.BAD_REQUEST
+            );
+        }
+    }
+
+    @GetMapping("/users/{name}")
+    public Optional<UserInfo> getUser(
+            @Nonnull User user,
+            @PathVariable("name") String name) {
+        // mask "not found" and "not allowed to see" as empty response
+        return winslow
+                .getUserManager()
+                .getUser(name)
+                .filter(userToBeLookedAt -> isAllowedToSeeGroup(user, userToBeLookedAt))
+                .map(UserInfoConverter::from);
+    }
+
+    private boolean isAllowedToSeeGroup(@Nullable User user, @Nonnull User toBeLookedAt) {
+        // ordered in ascending query complexity
+        return user != null && (user.hasSuperPrivileges() || toBeLookedAt.name().equals(user.name()));
     }
 }
