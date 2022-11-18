a95f0251e7856e4bbd27814085365d9b5967886e
Michael Watzko <michael.watzko@it-designers.de>
2022-11-18 16:03:37 +0100

    Add WINSLOW_AUTH_METHOD={ldap,local} and local AuthenticationProvider
    This also changes the default security behaviour a bit.
    
    None:
      If WINSLOW_DEV_ENV and WINSLOW_AUTH_METHOD and WINSLOW_LDAP_URL is not set,
      the default security plan disallows any access to the API.
    
    Ldap:
      If WINSLOW_DEV_ENV is not set and WINSLOW_LDAP_URL is set, or
      WINSLOW_AUTH_METHOD=ldap is set, the LDAP authentication is applied. Login
      requires a valid LDAP user.
    
    Local:
      If WINSLOW_AUTH_METHOD=local is set, the local authentication is applied.
      The login requires a valid local user. Currently, only the default user
      (root) and the WINSLOW_ROOT_USERS are recognized. Login is impossible due to
      missing passwords (to be updated/fixed).
    
    Dev:
      If WINSLOW_DEV_ENV is set and no WINSLOW_AUTH_METHOD is set, the dev
      environment mode is applied. This allows unauthenticated access to the API.

--- application/src/main/java/de/itdesigners/winslow/web/SecurityConfiguration.java
+++ application/src/main/java/de/itdesigners/winslow/web/SecurityConfiguration.java
@@ -1,15 +1,25 @@
 package de.itdesigners.winslow.web;
 
 import de.itdesigners.winslow.Env;
+import de.itdesigners.winslow.Winslow;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+
+import java.util.ArrayList;
+import java.util.Optional;
+import java.util.logging.Logger;
 
 import static de.itdesigners.winslow.web.webdav.WebDavController.EXPORT_NAME;
 
@@ -17,6 +27,7 @@ import static de.itdesigners.winslow.web.webdav.WebDavController.EXPORT_NAME;
 @EnableWebSecurity
 public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 
+    private static final Logger LOG = Logger.getLogger(SecurityConfiguration.class.getSimpleName());
 
     private @Value("${winslow.ldap.user.search-base:#{null}}")             String userSearchBase;
     private @Value("${winslow.ldap.user.search-filter:#{\"(uid={0})\"}}")  String userSearchFilter;
@@ -25,6 +36,8 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
     private @Value("${winslow.ldap.url:#{null}}")                          String ldapUrl;
     private @Value("${winslow.ldap.manager.dn:#{null}}")                   String managerDn;
     private @Value("${winslow.ldap.manager.password:#{null}}")             String managerPassword;
+
+    private @Autowired Winslow winslow;
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
@@ -40,7 +53,8 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
     }
 
     private void configureAuthorization(HttpSecurity http) throws Exception {
-        if (!Env.isDevEnv()) {
+        if (!Env.isDevEnv() || Env.isAuthMethodSet()) {
+            LOG.info("Authorization: Requiring login to access Winslow");
             /*
             http
                     .authorizeRequests()
@@ -53,7 +67,7 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
                     .authenticationEntryPoint(new BasicAuth());*/
             http
                     .authorizeRequests()
-                    .requestMatchers(new AntPathRequestMatcher(Env.getApiNoAuthPath()+"**")).permitAll()
+                    .requestMatchers(new AntPathRequestMatcher(Env.getApiNoAuthPath() + "**")).permitAll()
                     .and()
                     .authorizeRequests()
                     .anyRequest()
@@ -61,6 +75,8 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
                     .and()
                     .formLogin().and()
                     .httpBasic();
+        } else {
+            LOG.info("Authorization: No login required to access Winslow");
         }
     }
 
@@ -70,13 +86,14 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
         http.csrf().csrfTokenRepository(repo)
             .and().csrf().ignoringAntMatchers("/" + EXPORT_NAME + "/**")
             .and().csrf().ignoringAntMatchers(Env.getWebsocketPath() + "**")
-        .and().headers().frameOptions().sameOrigin();
+            .and().headers().frameOptions().sameOrigin();
     }
 
 
     @Autowired
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
         if (Env.isLdapAuthEnabled()) {
+            LOG.info("Authentication: Using LDAP authentication");
             auth.ldapAuthentication()
                 .userSearchBase(userSearchBase)
                 .userSearchFilter(userSearchFilter)
@@ -86,9 +103,53 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
                 .url(ldapUrl)
                 .managerDn(managerDn)
                 .managerPassword(managerPassword);
-        } else {
+        } else if (Env.isLocalAuthEnabled()) {
+            LOG.info("Authentication: Using built-in authentication");
+            auth.authenticationProvider(new AuthenticationProvider() {
+                @Override
+                public Authentication authenticate(Authentication authentication) throws AuthenticationException {
+                    return Optional
+                            .ofNullable(authentication.getCredentials())
+                            .filter(cred -> cred instanceof String)
+                            .map(Object::toString)
+                            .flatMap(pw -> winslow
+                                    .getUserManager()
+                                    .getUser(authentication.getName().trim())
+                                    .filter(user -> user.password() != null
+                                            && user.password().isPasswordCorrect(pw)
+                                    )
+                                    .map(user -> new UsernamePasswordAuthenticationToken(
+                                            user.name(),
+                                            null, // no need to store the password ... right?
+                                            new ArrayList<>()
+                                    )))
+                            .orElseThrow(() -> new BadCredentialsException("Invalid login"));
+                }
+
+
+                @Override
+                public boolean supports(Class<?> aClass) {
+                    return aClass != null;
+                }
+            });
+        } else if (Env.isDevEnv()) {
+            LOG.info("Authentication: No authentication in Dev-Environment");
             super.configure(auth);
+        } else {
+            LOG.warning("Unexpected auth method: " + Env.getAuthMethod());
+            auth.authenticationProvider(new AuthenticationProvider() {
+                @Override
+                public Authentication authenticate(Authentication authentication) throws AuthenticationException {
+                    throw new BadCredentialsException("Invalid auth method configured");
+                }
+
+                @Override
+                public boolean supports(Class<?> aClass) {
+                    return aClass != null;
+                }
+            });
         }
+
     }
 }
 
