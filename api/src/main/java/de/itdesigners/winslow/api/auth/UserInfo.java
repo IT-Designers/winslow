48c1b668d13fd60f1e7092e58e1e6b31ad04017b
Michael Watzko <michael.watzko@it-designers.de>
2022-11-18 16:23:49 +0100

    Add displayName and email to UserInfo (WEB-API)

--- api/src/main/java/de/itdesigners/winslow/api/auth/UserInfo.java
+++ api/src/main/java/de/itdesigners/winslow/api/auth/UserInfo.java
@@ -1,6 +1,11 @@
 package de.itdesigners.winslow.api.auth;
 
 import javax.annotation.Nonnull;
+import javax.annotation.Nullable;
 
-public record UserInfo(@Nonnull String name) {
+public record UserInfo(
+        @Nonnull String name,
+        @Nullable String displayName,
+        @Nullable String email
+) {
 }
