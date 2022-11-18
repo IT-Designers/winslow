14d50e4396a19606593b886230f2abeec8db34d4
Michael Watzko <michael.watzko@it-designers.de>
2022-11-18 11:52:50 +0100

    Rename class Security to SecurityConfiguration

--- application/src/main/java/de/itdesigners/winslow/web/SecurityConfiguration.java
+++ application/src/main/java/de/itdesigners/winslow/web/SecurityConfiguration.java
@@ -13,7 +13,7 @@ import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 @Configuration
 @EnableWebSecurity
-public class Security extends WebSecurityConfigurerAdapter {
+public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 
 
     private @Value("${winslow.ldap.user.search-base:#{null}}")             String userSearchBase;
