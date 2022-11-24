d7a5b6d6cec75c99e6ad962a17f7e6fe2bb2f3a2
ky4me6 <kyle.mezger@it-designers.de>
2022-11-24 12:53:08 +0100

    #18 #27 Add Interface to edit user

--- src/app/user-and-group-management/user-details/user-details.component.ts
+++ src/app/user-and-group-management/user-details/user-details.component.ts
@@ -1,24 +1,61 @@
-import {Component, EventEmitter, Input, OnInit, Output} from '@angular/core';
-import {UserInfo} from '../../api/user-api.service';
+import {Component, EventEmitter, Input, OnChanges, OnInit, Output, SimpleChanges} from '@angular/core';
+import {UserApiService, UserInfo} from '../../api/user-api.service';
+import {DialogService} from '../../dialog.service';
 
 @Component({
   selector: 'app-user-details',
   templateUrl: './user-details.component.html',
   styleUrls: ['./user-details.component.css']
 })
-export class UserDetailsComponent implements OnInit {
+export class UserDetailsComponent implements OnInit, OnChanges {
 
   @Input() selectedUser: UserInfo = null;
+  @Input() myName: string = null;
 
   @Output() deletedUserEmitter = new EventEmitter();
 
-  constructor() { }
+  canIEditUser = false;
+
+
+
+  constructor(private userApi: UserApiService, private dialog: DialogService) { }
 
   ngOnInit(): void {
   }
 
+  ngOnChanges(changes: SimpleChanges) {
+    if (this.myName === this.selectedUser.name) {
+      this.canIEditUser = true;
+    } else {
+      this.userApi.hasSuperPrivileges(this.myName)
+        .then((bool) => {
+          console.log('Am I a super user? ' + bool);
+          this.canIEditUser = bool;
+        });
+    }
+    console.log('Can i edit the user? ' + this.canIEditUser);
+  }
+
   onUserDelete() {
     this.deletedUserEmitter.emit(this.selectedUser);
+  }
+
+  canIUpdate() {
+    if (this.myName === this.selectedUser.name) {
+      this.canIEditUser = true;
+    } else {
+      this.userApi.hasSuperPrivileges(this.myName)
+        .then((bool) => {
+          console.log('Am I a super user? ' + bool);
+          this.canIEditUser = bool;
+        });
+    }
+  }
+
+  onUpdate() {
+    console.dir(this.selectedUser);
+    this.dialog.openLoadingIndicator(this.userApi.updateUser(this.selectedUser),
+      'Updating User');
   }
 
 }
