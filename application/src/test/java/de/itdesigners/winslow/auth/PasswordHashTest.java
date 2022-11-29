5bac9394591903ca6d3d8008ad6d81d6f8ecf834
Michael Watzko <michael.watzko@it-designers.de>
2022-11-29 13:23:22 +0100

    Add more regression tests for PasswordHash

--- application/src/test/java/de/itdesigners/winslow/auth/PasswordHashTest.java
+++ application/src/test/java/de/itdesigners/winslow/auth/PasswordHashTest.java
@@ -13,6 +13,27 @@ public class PasswordHashTest {
     }
 
     @Test
+    public void testPasswordHash01234567() {
+        assertTrue(PasswordHash.calculate("01234567").isPasswordCorrect("01234567"));
+    }
+
+    @Test
+    public void testPasswordHash0123456789() {
+        assertTrue(PasswordHash.calculate("0123456789").isPasswordCorrect("0123456789"));
+    }
+
+
+    @Test
+    public void testPasswordHash0123456789Wrong() {
+        assertFalse(PasswordHash.calculate("0123456788").isPasswordCorrect("0123456789"));
+    }
+
+    @Test
+    public void testPasswordHash01234567890123456789() {
+        assertTrue(PasswordHash.calculate("01234567890123456789").isPasswordCorrect("01234567890123456789"));
+    }
+
+    @Test
     public void testWrongPasswordHash() {
         assertFalse(PasswordHash.calculate("password").isPasswordCorrect("passwort"));
     }
