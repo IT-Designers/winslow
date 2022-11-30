6e723550f27c0727ee01f77873e5d6bd9dde09fb
ri4ho7 <rico.hofmann@it-designers.de>
2022-11-30 16:56:38 +0100

    Merge Fixes Complete

--- src/app/pipeline-view/pipeline-view.component.ts
+++ src/app/pipeline-view/pipeline-view.component.ts
@@ -11,26 +11,27 @@ import {
 } from '@angular/core';
 import {
   Action,
-  ConnectorPlacement, CreateEdgeAction,
-  CreateNodeAction, DeleteItemsAction,
+  ConnectorPlacement,
+  CreateEdgeAction,
+  CreateNodeAction,
+  DeleteItemsAction,
   DiagramMaker,
   DiagramMakerActions,
   DiagramMakerConfig,
   DiagramMakerData,
-  DiagramMakerEdge,
   DiagramMakerNode,
   DiagramMakerPotentialNode,
   Dispatch,
-  EditorMode, Layout, PositionAnchor, VisibleConnectorTypes, WorkflowLayoutDirection,
 } from 'diagram-maker';
 
-import {ImageInfo, ProjectInfo, StageDefinitionInfo} from "../api/project-api.service";
+
 import {DiagramNodeComponent} from "./diagram-node/diagram-node.component";
 import {DiagramLibraryComponent} from "./diagram-library/diagram-library.component";
 import {DiagramConfigHelper} from "./diagram-config-helper";
 import {DiagramInitialData} from "./diagram-initial-data";
 import {AddToolsComponent} from "./add-tools/add-tools.component";
 import {DiagramGatewayComponent} from "./diagram-gateway/diagram-gateway.component";
+import {ImageInfo, ProjectInfo, StageDefinitionInfo} from "../api/winslow-api";
 
 @Component({
   selector: 'app-pipeline-view',
@@ -114,12 +115,13 @@ export class PipelineViewComponent implements OnInit, AfterViewInit, OnDestroy {
         const createAction = action as CreateNodeAction<any>;
         if (createAction.payload.typeId == "node-normal" || createAction.payload.typeId ==  "node-start"){
 
-          const stageDef = new StageDefinitionInfo();
-          stageDef.name = "New Stage";
-          stageDef.image = new ImageInfo();
-          stageDef.requiredEnvVariables = [];
-          stageDef.requiredResources = null;
-          stageDef.env = new Map;
+          const stageDef = new StageDefinitionInfo({
+            id: "",
+            name: "New Stage",
+            image: new ImageInfo({}),
+            requiredEnvVariables: [],
+            env: {}
+          });
           //console.log(stageDef);
           this.project.pipelineDefinition.stages.push(stageDef)
           let newAction: CreateNodeAction<{}> = {
@@ -139,8 +141,7 @@ export class PipelineViewComponent implements OnInit, AfterViewInit, OnDestroy {
           createAction.payload.typeId == "node-all-merger" ||
           createAction.payload.typeId == "node-any-merger"
         ){
-          const stageDef = new StageDefinitionInfo();
-          stageDef.name = `${createAction.payload.typeId}`;
+          const stageDef = new StageDefinitionInfo({id: "", name: createAction.payload.typeId, requiredEnvVariables: []});
           let newAction: CreateNodeAction<{}> = {
             type: DiagramMakerActions.NODE_CREATE,
             payload: {
