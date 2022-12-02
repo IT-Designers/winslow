726d8f99ba0d91e4208af3d734b75a8b8ced46b7
Stefan Kaufmann <stefan.kaufmann@it-designers.de>
2022-12-02 12:06:20 +0100

    replace toml with yaml serialisation

--- application/src/test/java/de/itdesigners/winslow/config/UserInputTests.java
+++ application/src/test/java/de/itdesigners/winslow/config/UserInputTests.java
@@ -1,7 +1,10 @@
 package de.itdesigners.winslow.config;
 
-import com.moandjiezana.toml.Toml;
+
+import de.itdesigners.winslow.BaseRepository;
 import org.junit.Test;
+
+import java.io.IOException;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
@@ -9,34 +12,29 @@ import static org.junit.Assert.assertTrue;
 public class UserInputTests {
 
     @Test
-    public void testConfirmationMappingNever() {
-        var userInput = new Toml().read("[userInput]\n" +
-                "confirmation = \"Never\""
-        )
-                .getTable("userInput")
-                .to(UserInput.class);
+    public void testConfirmationMappingNever() throws IOException {
+
+        var userInputYaml = """ 
+                confirmation: "Never" 
+                 """;
+
+
+        var userInput = BaseRepository.readFromString(UserInput.class, userInputYaml);
 
         assertTrue(userInput.getEnvironment().isEmpty());
         assertEquals(UserInput.Confirmation.Never, userInput.getConfirmation());
     }
 
-    @Test
-    public void testConfirmationMappingDefault() {
-        var userInput = new Toml().read("[userInput]")
-                .getTable("userInput")
-                .to(UserInput.class);
-
-        assertTrue(userInput.getEnvironment().isEmpty());
-        assertEquals(UserInput.Confirmation.Never, userInput.getConfirmation());
-    }
 
     @Test
-    public void testConfirmationMappingNonDefault() {
-        var userInput = new Toml().read("[userInput]\n" +
-                "confirmation = \"Always\""
-        )
-                .getTable("userInput")
-                .to(UserInput.class);
+    public void testConfirmationMappingNonDefault() throws IOException {
+
+
+        var userInputYaml = """ 
+                confirmation: "Always" 
+                 """;
+
+        var userInput = BaseRepository.readFromString(UserInput.class, userInputYaml);
 
         assertTrue(userInput.getEnvironment().isEmpty());
         assertEquals(UserInput.Confirmation.Always, userInput.getConfirmation());
