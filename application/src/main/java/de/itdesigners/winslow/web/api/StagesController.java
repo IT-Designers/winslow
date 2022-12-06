f4f69b792f967e2133752341378654f6ba36d21e
Stefan Kaufmann <stefan.kaufmann@it-designers.de>
2022-12-06 14:17:01 +0100

    add /stages/default/-web api

--- application/src/main/java/de/itdesigners/winslow/web/api/StagesController.java
+++ application/src/main/java/de/itdesigners/winslow/web/api/StagesController.java
@@ -3,6 +3,9 @@ package de.itdesigners.winslow.web.api;
 import de.itdesigners.winslow.Winslow;
 import de.itdesigners.winslow.api.pipeline.StageDefinitionInfo;
 import de.itdesigners.winslow.api.pipeline.StageWorkerDefinitionInfo;
+import de.itdesigners.winslow.config.StageAndGatewayDefinition;
+import de.itdesigners.winslow.config.StageWorkerDefinition;
+import de.itdesigners.winslow.config.StageXOrGatwayDefinition;
 import de.itdesigners.winslow.web.StageDefinitionInfoConverter;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -17,6 +20,52 @@ public class StagesController {
 
     public StagesController(Winslow winslow) {
         this.winslow = winslow;
+    }
+
+    @GetMapping("/stages/default/worker")
+    public StageDefinitionInfo getDefaultWorker() {
+
+        return StageDefinitionInfoConverter.from(new StageWorkerDefinition(
+                null,
+                "worker",
+                null,
+                null,
+                null,
+                null,
+                null,
+                null,
+                null,
+                null,
+                null,
+                null,
+                null
+        ));
+
+    }
+
+    @GetMapping("/stages/default/andGateway")
+    public StageDefinitionInfo getDefaultAndGateway() {
+
+        return StageDefinitionInfoConverter.from(new StageAndGatewayDefinition(
+                null,
+                "worker",
+                null,
+                null
+        ));
+
+    }
+
+    @GetMapping("/stages/default/xorGateway")
+    public StageDefinitionInfo getDefaultXOrGateway() {
+
+        return StageDefinitionInfoConverter.from(new StageXOrGatwayDefinition(
+                null,
+                "worker",
+                null,
+                null,
+                null
+        ));
+
     }
 
     @GetMapping("/stages/{pipeline}")
