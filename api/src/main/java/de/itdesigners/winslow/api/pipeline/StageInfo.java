07e8bf0dae0f2b459d218c160c9fc1228eac5486
Michael Watzko <michael.watzko@it-designers.de>
2022-12-20 15:09:54 +0100

    Convert StageInfo to record

--- api/src/main/java/de/itdesigners/winslow/api/pipeline/StageInfo.java
+++ api/src/main/java/de/itdesigners/winslow/api/pipeline/StageInfo.java
@@ -2,42 +2,44 @@ package de.itdesigners.winslow.api.pipeline;
 
 import javax.annotation.Nonnull;
 import javax.annotation.Nullable;
+import java.beans.Transient;
 import java.util.Date;
 import java.util.Map;
+import java.util.Optional;
 
-public class StageInfo {
+public record StageInfo(
+        @Nonnull String id,
+        @Nullable Date startTime,
+        @Nullable Date finishTime,
+        @Nullable State state,
+        @Nullable String workspace,
+        @Nonnull Map<String, String> env,
+        @Nonnull Map<String, String> envPipeline,
+        @Nonnull Map<String, String> envSystem,
+        @Nonnull Map<String, String> envInternal,
+        @Nonnull Map<String, String> result) {
 
-    public final @Nonnull  String              id;
-    public final @Nullable Date                startTime;
-    public final @Nullable Date                finishTime;
-    public final @Nullable State               state;
-    public final @Nullable String              workspace;
-    public final @Nonnull  Map<String, String> env;
-    public final @Nonnull  Map<String, String> envPipeline;
-    public final @Nonnull  Map<String, String> envSystem;
-    public final @Nonnull  Map<String, String> envInternal;
-    public final @Nonnull  Map<String, String> result;
+    @Nonnull
+    @Transient
+    public Optional<Date> optStartTime() {
+        return Optional.ofNullable(startTime);
+    }
 
-    public StageInfo(
-            @Nonnull String id,
-            @Nullable Date startTime,
-            @Nullable Date finishTime,
-            @Nullable State state,
-            @Nullable String workspace,
-            @Nonnull Map<String, String> env,
-            @Nonnull Map<String, String> envPipeline,
-            @Nonnull Map<String, String> envSystem,
-            @Nonnull Map<String, String> envInternal,
-            @Nonnull Map<String, String> result) {
-        this.id          = id;
-        this.startTime   = startTime;
-        this.finishTime  = finishTime;
-        this.state       = state;
-        this.workspace   = workspace;
-        this.env         = env;
-        this.envPipeline = envPipeline;
-        this.envSystem   = envSystem;
-        this.envInternal = envInternal;
-        this.result      = result;
+    @Nonnull
+    @Transient
+    public Optional<Date> optFinishTime() {
+        return Optional.ofNullable(finishTime);
+    }
+
+    @Nonnull
+    @Transient
+    public Optional<State> optState() {
+        return Optional.ofNullable(state);
+    }
+
+    @Nonnull
+    @Transient
+    public Optional<String> optWorkspace() {
+        return Optional.ofNullable(workspace);
     }
 }
