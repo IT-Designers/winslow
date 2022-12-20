45fca8d91a3779388cf8f00619e4b385237ec6d8
Michael Watzko <michael.watzko@it-designers.de>
2022-12-20 17:58:00 +0100

    Regenerate winslow-api.ts (PipelineDefinitionInfo)

--- src/app/api/winslow-api.ts
+++ src/app/api/winslow-api.ts
@@ -1,6 +1,6 @@
 /* tslint:disable */
 /* eslint-disable */
-// Generated using typescript-generator version 3.1.1185 on 2022-12-20 14:56:20.
+// Generated using typescript-generator version 3.1.1185 on 2022-12-20 17:54:59.
 
 export class GroupInfo {
   name: string;
@@ -352,7 +352,7 @@ export class ParseError {
 export class PipelineDefinitionInfo {
   id: string;
   name: string;
-  description?: string;
+  description: string;
   userInput: UserInputInfo;
   stages: StageDefinitionInfoUnion[];
   deletionPolicy: DeletionPolicy;
