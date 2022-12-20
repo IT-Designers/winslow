5e800bb3844b2e55fd48561340f241a172fb3099
Michael Watzko <michael.watzko@it-designers.de>
2022-12-20 14:47:50 +0100

    Convert WorkspaceConfiguration to record

--- api/src/main/java/de/itdesigners/winslow/api/pipeline/WorkspaceConfiguration.java
+++ api/src/main/java/de/itdesigners/winslow/api/pipeline/WorkspaceConfiguration.java
@@ -1,55 +1,59 @@
 package de.itdesigners.winslow.api.pipeline;
 
+import javax.annotation.CheckReturnValue;
 import javax.annotation.Nonnull;
 import javax.annotation.Nullable;
 import java.beans.ConstructorProperties;
+import java.beans.Transient;
 import java.util.Optional;
 
-public class WorkspaceConfiguration {
-
-    private final @Nonnull  WorkspaceMode mode;
-    private final @Nullable String        value;
-    private final           boolean sharedWithinGroup;
-    private final           boolean nestedWithinGroup;
-
+public record WorkspaceConfiguration(
+        @Nonnull WorkspaceMode mode,
+        @Nullable String value,
+        boolean sharedWithinGroup,
+        boolean nestedWithinGroup
+) {
     public WorkspaceConfiguration() {
-        this(WorkspaceMode.INCREMENTAL, null, null, null);
+        this(WorkspaceMode.INCREMENTAL, null, false, false);
+    }
+
+    public WorkspaceConfiguration(@Nonnull WorkspaceMode mode) {
+        this(mode, null, false, false);
     }
 
     @ConstructorProperties({"mode", "value", "sharedWithinGroup", "nestedWithinGroup"})
-    public WorkspaceConfiguration(
-            @Nonnull WorkspaceMode mode,
-            @Nullable String value,
-            @Nullable Boolean sharedWithinGroup,
-            @Nullable Boolean nestedWithinGroup) {
-        this.mode              = mode;
-        this.value             = value;
-        this.sharedWithinGroup = sharedWithinGroup != null && sharedWithinGroup;
-        this.nestedWithinGroup = nestedWithinGroup != null && nestedWithinGroup;
+    public WorkspaceConfiguration {
     }
 
     @Nonnull
-    public WorkspaceMode getMode() {
-        return mode;
-    }
-
-    @Nonnull
-    public Optional<String> getValue() {
+    @Transient
+    public Optional<String> optValue() {
         return Optional.ofNullable(value);
     }
 
-    public boolean isSharedWithinGroup() {
-        return sharedWithinGroup;
-    }
-
     /**
-     * This option has no effect, if {@link #isSharedWithinGroup()} is set to true and will in fact never return
-     * true in that case.
+     * Only returns true if {@link #sharedWithinGroup()} is false and {@link #nestedWithinGroup} is true.
+     *
      * @return Whether the multiple workspaces should be nested within the workspace of the group.
      */
-    public boolean isNestedWithinGroup() {
+    @Transient
+    public boolean nestedWithinGroupExclusive() {
         // TODO controversial: applying (business-)logic rules in getter?
-        return nestedWithinGroup && !isSharedWithinGroup();
+        return nestedWithinGroup && !sharedWithinGroup();
+    }
+
+    @Nonnull
+    @Transient
+    @CheckReturnValue
+    public WorkspaceConfiguration withSharedWithinGroupExclusively() {
+        return new WorkspaceConfiguration(mode, value, true, false);
+    }
+
+    @Nonnull
+    @Transient
+    @CheckReturnValue
+    public WorkspaceConfiguration withNestedWithinGroupExclusively() {
+        return new WorkspaceConfiguration(mode, value, false, true);
     }
 
     public enum WorkspaceMode {
