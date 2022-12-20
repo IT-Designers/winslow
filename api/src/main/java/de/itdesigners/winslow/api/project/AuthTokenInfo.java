88490a4e0f6bd92dd36cc78cc3e6a34112ec062b
Michael Watzko <michael.watzko@it-designers.de>
2022-12-20 14:34:42 +0100

    Convert AuthTokenInfo to record

--- api/src/main/java/de/itdesigners/winslow/api/project/AuthTokenInfo.java
+++ api/src/main/java/de/itdesigners/winslow/api/project/AuthTokenInfo.java
@@ -1,31 +1,28 @@
 package de.itdesigners.winslow.api.project;
 
+import javax.annotation.CheckReturnValue;
 import javax.annotation.Nonnull;
 import javax.annotation.Nullable;
+import java.beans.Transient;
 import java.util.List;
+import java.util.Optional;
 
-public class AuthTokenInfo {
+public record AuthTokenInfo(
+        @Nonnull String id,
+        @Nullable String secret,
+        @Nonnull String name,
+        @Nonnull List<String> capabilities) {
 
-    public final @Nonnull String       id;
-    public @Nullable      String       secret;
-    public final @Nonnull String       name;
-    public final @Nonnull List<String> capabilities;
-
-    public AuthTokenInfo(
-            @Nonnull String id,
-            @Nullable String secret,
-            @Nonnull String name,
-            @Nonnull List<String> capabilities
-    ) {
-        this.id           = id;
-        this.secret       = secret;
-        this.name         = name;
-        this.capabilities = capabilities;
+    @Nonnull
+    @Transient
+    public Optional<String> optSecret() {
+        return Optional.ofNullable(secret);
     }
 
     @Nonnull
+    @Transient
+    @CheckReturnValue
     public AuthTokenInfo withSecret(@Nonnull String secret) {
-        this.secret = secret;
-        return this;
+        return new AuthTokenInfo(this.id, secret, this.name, this.capabilities);
     }
 }
