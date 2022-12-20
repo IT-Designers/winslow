48f830d7d9146ab2e4ec1612cf1915f0e712e72f
Michael Watzko <michael.watzko@it-designers.de>
2022-12-20 10:07:01 +0100

    Convert StorageInfo to a record

--- api/src/main/java/de/itdesigners/winslow/api/storage/StorageInfo.java
+++ api/src/main/java/de/itdesigners/winslow/api/storage/StorageInfo.java
@@ -1,17 +1,10 @@
 package de.itdesigners.winslow.api.storage;
 
 import javax.annotation.Nonnull;
-import javax.annotation.Nullable;
 
-public class StorageInfo {
+public record StorageInfo(
+        @Nonnull String name,
+        long bytesUsed,
+        long bytesFree) {
 
-    public final @Nullable String name;
-    public final           long   bytesUsed;
-    public final           long   bytesFree;
-
-    public StorageInfo(@Nonnull String name, long bytesUsed, long bytesFree) {
-        this.name      = name;
-        this.bytesUsed = bytesUsed;
-        this.bytesFree = bytesFree;
-    }
 }
