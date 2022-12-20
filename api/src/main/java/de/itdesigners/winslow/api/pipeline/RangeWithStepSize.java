e6088f3a26d272e7440288d9b36930ad86e34f4c
Michael Watzko <michael.watzko@it-designers.de>
2022-12-20 15:15:03 +0100

    Convert RangeWithStepSize to record

--- api/src/main/java/de/itdesigners/winslow/api/pipeline/RangeWithStepSize.java
+++ api/src/main/java/de/itdesigners/winslow/api/pipeline/RangeWithStepSize.java
@@ -3,29 +3,16 @@ package de.itdesigners.winslow.api.pipeline;
 import java.beans.ConstructorProperties;
 import java.beans.Transient;
 
-public class RangeWithStepSize implements RangedValue {
-
-    private final float min;
-    private final float max;
-    private final float stepSize;
+public record RangeWithStepSize(
+        float min,
+        float max,
+        float stepSize) implements RangedValue {
 
     @ConstructorProperties({"min", "max", "stepSize"})
     public RangeWithStepSize(float min, float max, float stepSize) {
         this.min      = Math.min(min, max);
         this.max      = Math.max(min, max);
         this.stepSize = Math.abs(stepSize);
-    }
-
-    public float getMin() {
-        return min;
-    }
-
-    public float getMax() {
-        return max;
-    }
-
-    public float getStepSize() {
-        return stepSize;
     }
 
     @Transient
@@ -42,7 +29,7 @@ public class RangeWithStepSize implements RangedValue {
         var result  = Math.min(this.min + current, this.max);
 
         if (isIntegerRange()) {
-            return String.valueOf((int)result);
+            return String.valueOf((int) result);
         } else {
             return String.valueOf(result);
         }
