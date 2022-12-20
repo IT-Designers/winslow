e98b9e2ee56cb7461c704449ddedaeee7a6d6f4e
Michael Watzko <michael.watzko@it-designers.de>
2022-12-20 18:12:06 +0100

    Convert ParseError to record

--- api/src/main/java/de/itdesigners/winslow/api/pipeline/ParseError.java
+++ api/src/main/java/de/itdesigners/winslow/api/pipeline/ParseError.java
@@ -1,14 +1,10 @@
 package de.itdesigners.winslow.api.pipeline;
 
-public class ParseError {
+import javax.annotation.Nonnull;
 
-    public final int    line;
-    public final int    column;
-    public final String message;
+public record ParseError(
+        int line,
+        int column,
+        @Nonnull String message) {
 
-    public ParseError(int line, int column, String message) {
-        this.line    = line;
-        this.column  = column;
-        this.message = message;
-    }
 }
