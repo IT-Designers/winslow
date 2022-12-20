e0ef9c00ce01efdab804117afa7188f3848a58c9
Michael Watzko <michael.watzko@it-designers.de>
2022-12-20 13:51:01 +0100

    Convert UpdatePauseRequest to record

--- api/src/main/java/de/itdesigners/winslow/api/project/UpdatePauseRequest.java
+++ api/src/main/java/de/itdesigners/winslow/api/project/UpdatePauseRequest.java
@@ -1,13 +1,9 @@
 package de.itdesigners.winslow.api.project;
 
-import javax.annotation.Nullable;
+import javax.annotation.Nonnull;
+import java.util.Optional;
 
-public class UpdatePauseRequest {
-    public           boolean paused;
-    public @Nullable String  strategy;
-
-    public UpdatePauseRequest(boolean paused, @Nullable String strategy) {
-        this.paused   = paused;
-        this.strategy = strategy;
-    }
+public record UpdatePauseRequest(
+        boolean paused,
+        @Nonnull Optional<String> strategy) {
 }
