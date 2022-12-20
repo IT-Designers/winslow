d74dbd633b65756d573f3f33e53c803ca176e306
Michael Watzko <michael.watzko@it-designers.de>
2022-12-20 15:03:38 +0100

    Convert StateInfo to record

--- api/src/main/java/de/itdesigners/winslow/api/pipeline/StateInfo.java
+++ api/src/main/java/de/itdesigners/winslow/api/pipeline/StateInfo.java
@@ -1,46 +1,39 @@
 package de.itdesigners.winslow.api.pipeline;
 
+import javax.annotation.Nonnull;
 import javax.annotation.Nullable;
-import java.util.Objects;
+import java.beans.Transient;
+import java.util.Optional;
 
-public class StateInfo {
+public record StateInfo(
+        @Nullable State state,
+        @Nullable String pauseReason,
+        @Nullable String description,
+        @Nullable Integer stageProgress,
+        boolean hasEnqueuedStages) {
 
-    public final @Nullable State   state;
-    public final @Nullable String  pauseReason;
-    public final @Nullable String  description;
-    public final @Nullable Integer stageProgress;
-    public final           boolean hasEnqueuedStages;
-
-
-    public StateInfo(
-            @Nullable State state,
-            @Nullable String pauseReason,
-            @Nullable String description,
-            @Nullable Integer stageProgress,
-            boolean hasEnqueuedStages) {
-        this.state             = state;
-        this.pauseReason       = pauseReason;
-        this.description       = description;
-        this.stageProgress     = stageProgress;
-        this.hasEnqueuedStages = hasEnqueuedStages;
+    @Nonnull
+    @Transient
+    public Optional<State> optState() {
+        return Optional.ofNullable(state);
     }
 
-    @Override
-    public boolean equals(Object o) {
-        if (this == o)
-            return true;
-        if (o == null || getClass() != o.getClass())
-            return false;
-        StateInfo stateInfo = (StateInfo) o;
-        return hasEnqueuedStages == stateInfo.hasEnqueuedStages &&
-                state == stateInfo.state &&
-                Objects.equals(pauseReason, stateInfo.pauseReason) &&
-                Objects.equals(description, stateInfo.description) &&
-                Objects.equals(stageProgress, stateInfo.stageProgress);
+    @Nonnull
+    @Transient
+    public Optional<String> optPauseReason() {
+        return Optional.ofNullable(pauseReason);
     }
 
-    @Override
-    public int hashCode() {
-        return Objects.hash(state, pauseReason, description, stageProgress, hasEnqueuedStages);
+    @Nonnull
+    @Transient
+    public Optional<String> optDescription() {
+        return Optional.ofNullable(description);
     }
+
+    @Nonnull
+    @Transient
+    public Optional<Integer> optStageProgress() {
+        return Optional.ofNullable(stageProgress);
+    }
+
 }
