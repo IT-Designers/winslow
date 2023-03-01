0a73b07d3d8210a0af01810c91dfd93bbc97bbea
Michael Watzko <michael.watzko@it-designers.de>
2023-03-01 16:25:38 +0100

    Add NodeResourceUsageConfiguration and GroupResourceLimitEntry #208 #209

---
+++ api/src/main/java/de/itdesigners/winslow/api/node/NodeResourceUsageConfiguration.java
@@ -0,0 +1,29 @@
+package de.itdesigners.winslow.api.node;
+
+import de.itdesigners.winslow.api.settings.ResourceLimitation;
+
+import javax.annotation.Nonnull;
+import java.util.Map;
+import java.util.Optional;
+
+/**
+ * The resource limit of a node can be configured in two modi:
+ *
+ * <ul>
+ *     <li>
+ *         If {@link #freeForAll} is configured, the configured node that can be used by anybody without being mentioned
+ *         explicitly. A global upper resource usage limit may be applied anyway.
+ *     </li>
+ *     <li>
+ *         If {@link #freeForAll} is not configured, limitations apply as configured individually and no access is
+ *         granted without the group or user being mentioned explicitly (except privileged users).
+ *     </li>
+ * </ul>
+ */
+public record NodeResourceUsageConfiguration(
+        boolean freeForAll,
+        @Nonnull Optional<ResourceLimitation> globalLimit,
+        @Nonnull Map<String, GroupResourceLimitEntry> groupLimits
+) {
+}
+
