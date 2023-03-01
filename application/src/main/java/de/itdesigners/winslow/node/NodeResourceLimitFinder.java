a23f8f7ccbd6cf1940361e3960950e3aa11c5c8f
Michael Watzko <michael.watzko@it-designers.de>
2023-03-01 16:27:30 +0100

    Consider NodeResourceUsageConfiguration before participating in elections #208 #209

---
+++ application/src/main/java/de/itdesigners/winslow/node/NodeResourceLimitFinder.java
@@ -0,0 +1,80 @@
+package de.itdesigners.winslow.node;
+
+import de.itdesigners.winslow.api.node.GroupResourceLimitEntry;
+import de.itdesigners.winslow.api.node.NodeResourceUsageConfiguration;
+import de.itdesigners.winslow.api.settings.ResourceLimitation;
+import de.itdesigners.winslow.auth.Group;
+import de.itdesigners.winslow.auth.User;
+
+import javax.annotation.Nonnull;
+import java.util.Collections;
+import java.util.Map;
+import java.util.Objects;
+import java.util.Optional;
+
+/**
+ * See {@link NodeResourceUsageConfiguration}
+ */
+public record NodeResourceLimitFinder(@Nonnull NodeResourceUsageConfiguration configuration) {
+
+    public static final @Nonnull ResourceLimitation             LIMIT_ZERO          = new ResourceLimitation(
+            0L, 0L, 0L
+    );
+    public static final @Nonnull NodeResourceUsageConfiguration ONLY_FOR_PRIVILEGED = new NodeResourceUsageConfiguration(
+            false, Optional.empty(), Collections.emptyMap()
+    );
+
+    /**
+     * @return The fallback value for scenarios without a recognized {@link User}
+     */
+    @Nonnull
+    public Optional<ResourceLimitation> getFallback() {
+        if (configuration.freeForAll()) {
+            return configuration.globalLimit();
+        } else {
+            return Optional.of(LIMIT_ZERO);
+        }
+    }
+
+    @Nonnull
+    public Optional<ResourceLimitation> getAppliedLimit(@Nonnull User user) {
+        if (user.hasSuperPrivileges()) {
+            return Optional.empty();
+        } else {
+            return user
+                    .getGroups()
+                    .stream()
+                    .map(this::getAppliedLimit)
+                    .reduce(Optional.of(LIMIT_ZERO), (a, b) -> {
+                        if (a.isPresent() && b.isPresent()) {
+                            return Optional.of(a.get().max(b.get()));
+                        } else {
+                            // at least for one group there is unrestricted access
+                            return Optional.empty();
+                        }
+                    });
+        }
+    }
+
+    @Nonnull
+    public Optional<ResourceLimitation> getAppliedLimit(@Nonnull Group group) {
+        if (group.isSuperGroup()) {
+            return Optional.empty();
+        } else if (configuration.freeForAll()) {
+            return configuration.globalLimit();
+        } else {
+            return Optional.of(
+                    configuration
+                            .groupLimits()
+                            .entrySet()
+                            .stream()
+                            .filter(e -> Objects.equals(e.getKey(), group.name()))
+                            .map(Map.Entry::getValue)
+                            .map(GroupResourceLimitEntry::resourceLimitation)
+                            .findFirst()
+                            // no entry means no access
+                            .orElse(LIMIT_ZERO)
+            );
+        }
+    }
+}
