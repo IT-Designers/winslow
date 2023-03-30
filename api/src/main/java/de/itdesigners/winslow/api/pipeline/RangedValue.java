37a9392e57b50365a57c82b34175ff9f0d843b76
Michael Watzko <michael.watzko@it-designers.de>
2023-03-30 19:40:48 +0200

    Revert "Revert "No longer create a wrapper object for serialized RangedValue""
    This reverts commit 221ad04b6f65d62a600bc9575c4238c9643eef27.

--- api/src/main/java/de/itdesigners/winslow/api/pipeline/RangedValue.java
+++ api/src/main/java/de/itdesigners/winslow/api/pipeline/RangedValue.java
@@ -4,8 +4,7 @@ import com.fasterxml.jackson.annotation.JsonSubTypes;
 import com.fasterxml.jackson.annotation.JsonTypeInfo;
 
 @JsonTypeInfo(
-        use = JsonTypeInfo.Id.NAME,
-        include = JsonTypeInfo.As.WRAPPER_OBJECT
+        use = JsonTypeInfo.Id.NAME
 )
 @JsonSubTypes({
         @JsonSubTypes.Type(value = RangeWithStepSize.class, name = "DiscreteSteps"),
