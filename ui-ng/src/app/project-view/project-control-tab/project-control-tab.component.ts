9fd1db1ab285a75e9767f7a973ca65020f153ccb
Thorsten Rausch <thorsten.rausch@it-designers.de>
2023-10-10 15:44:34 +0200

    Create project-control-tab component

---
+++ ui-ng/src/app/project-view/project-control-tab/project-control-tab.component.ts
@@ -0,0 +1,121 @@
+import {Component, Input, ViewChild} from '@angular/core';
+import {
+  EnvVariable, ExecutionGroupInfo, ImageInfo,
+  PipelineDefinitionInfo, ProjectInfo,
+  RangedValue, ResourceInfo,
+  StageDefinitionInfo, StageWorkerDefinitionInfo, WorkspaceConfiguration,
+  WorkspaceMode
+} from "../../api/winslow-api";
+import {StageExecutionSelectionComponent} from "../../stage-execution-selection/stage-execution-selection.component";
+import {DialogService} from "../../dialog.service";
+import {ProjectApiService} from "../../api/project-api.service";
+import {
+  GroupSettingsDialogComponent,
+  GroupSettingsDialogData
+} from "../../group-settings-dialog/group-settings-dialog.component";
+import {MatDialog, MatDialogState} from "@angular/material/dialog";
+
+@Component({
+  selector: 'app-project-control-tab',
+  templateUrl: './project-control-tab.component.html',
+  styleUrls: ['./project-control-tab.component.css']
+})
+export class ProjectControlTabComponent {
+
+  @ViewChild('executionSelection') executionSelection: StageExecutionSelectionComponent;
+  selectedProject: ProjectInfo;
+
+  @Input() set project(project: ProjectInfo) {
+    this.selectedProject = project;
+  }
+
+  protected readonly history: ExecutionGroupInfo[];
+  environmentVariables: Map<string, EnvVariable>;
+  defaultEnvironmentVariables: Record<string, string>;
+  rangedEnvironmentVariables: Record<string, RangedValue>;
+  workspaceConfigurationMode: WorkspaceMode;
+
+  constructor(
+    private matDialog: MatDialog,
+    private dialog: DialogService,
+    private projectApi: ProjectApiService,
+  ) {
+  }
+
+  // todo
+  configure(pipeline: PipelineDefinitionInfo, stage: StageDefinitionInfo, env: any, image: ImageInfo, requiredResources?: ResourceInfo) {
+    if (pipeline.name === this.project.pipelineDefinition.name) {
+      let index = null;
+      for (let i = 0; i < pipeline.stages.length; ++i) {
+        if (pipeline.stages[i].name === stage.name) {
+          index = i;
+          break;
+        }
+      }
+      if (index !== null) {
+        this.dialog.openLoadingIndicator(
+          this.api.configureGroup(this.project.id, index, [this.project.id], env, image, requiredResources),
+          `Submitting selections`
+        );
+      }
+    } else {
+      this.dialog.error('Changing the Pipeline is not yet supported!');
+    }
+  }
+
+  // todo
+  configureGroup(pipeline: PipelineDefinitionInfo, stage: StageDefinitionInfo, env: any, image: ImageInfo) {
+    for (let i = 0; i < pipeline.stages.length; ++i) {
+      if (stage.name === pipeline.stages[i].name) {
+        return this.api.listProjects()
+          .then(projects => {
+            return this.matDialog
+              .open(GroupSettingsDialogComponent, {
+                data: {
+                  projects,
+                  availableTags: this.api.cachedTags,
+                } as GroupSettingsDialogData
+              })
+              .afterClosed()
+              .toPromise()
+              .then((selectedProjects: ProjectInfo[] | null) => {
+                if (selectedProjects) {
+                  const selectedProjectIds = selectedProjects.map(project => project.id)
+                  return this.dialog.openLoadingIndicator(
+                    this.api.configureGroup(this.project.id, i, selectedProjectIds, env, image)
+                      .then(configureResult => {
+                        const failed = [];
+                        for (let n = 0; n < configureResult.length && n < selectedProjectIds.length; ++n) {
+                          if (!configureResult[n]) {
+                            failed.push(selectedProjectIds[n]);
+                          }
+                        }
+                        if (failed.length === 0) {
+                          return Promise.resolve();
+                        } else {
+                          return Promise.reject('The operation failed for at least one project: ' + (failed.join(', ')));
+                        }
+                      }),
+                    `Applying settings on all selected projects`,
+                  );
+                }
+              });
+          });
+      }
+    }
+  }
+
+  enqueue(
+    pipeline: PipelineDefinitionInfo,
+    stageDefinitionInfo: StageDefinitionInfo,
+    env: any,
+    rangedEnv: any,
+    image: ImageInfo,
+    requiredResources?: ResourceInfo,
+    workspaceConfiguration?: WorkspaceConfiguration,
+    comment?: string,
+    runSingle?: boolean,
+    resume?: boolean,
+  ) {
+  }
+}
