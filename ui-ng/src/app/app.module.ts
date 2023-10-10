28d9c0028f7e2c21faae10ce88aac2b46153fd62
Thorsten Rausch <thorsten.rausch@it-designers.de>
2023-10-10 15:44:34 +0200

    Create project-control-tab component

--- ui-ng/src/app/app.module.ts
+++ ui-ng/src/app/app.module.ts
@@ -112,6 +112,7 @@ import {MatSliderModule} from '@angular/material/slider';
 import { ServerGroupsListComponent } from './server-details/server-groups-list/server-groups-list.component';
 import { AddPipelineDialogComponent } from './pipelines/add-pipeline-dialog/add-pipeline-dialog.component';
 import { PipelineDetailsComponent } from './pipelines/pipeline-details/pipeline-details.component';
+import { ProjectControlTabComponent } from './project-view/project-control-tab/project-control-tab.component';
 
 @NgModule({
     declarations: [
@@ -188,6 +189,7 @@ import { PipelineDetailsComponent } from './pipelines/pipeline-details/pipeline-
         ServerGroupsListComponent,
         AddPipelineDialogComponent,
         PipelineDetailsComponent,
+        ProjectControlTabComponent,
     ],
     imports: [
         SweetAlert2Module.forRoot(),
