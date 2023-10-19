b894b861cf7f1c50a5237d5e2c9e3da638476b39
Thorsten Rausch <thorsten.rausch@it-designers.de>
2023-10-19 16:43:35 +0200

    Fix type issues in subscription-handler

--- src/app/api/subscription-cache.spec.ts
+++ src/app/api/subscription-cache.spec.ts
@@ -1,7 +1,8 @@
 import { SubscriptionHandler } from './subscription-handler';
+import {RxStompService} from "../rx-stomp.service";
 
 describe('SubscriptionCache', () => {
   it('should create an instance', () => {
-    expect(new SubscriptionHandler()).toBeTruthy();
+    expect(new SubscriptionHandler(new RxStompService(), "")).toBeTruthy();
   });
 });
