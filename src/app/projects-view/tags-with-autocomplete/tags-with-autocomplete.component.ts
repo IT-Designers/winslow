8905ef9c8f523d70bfaa7945d7fe2ddef4df5012
Thorsten Rausch <thorsten.rausch@it-designers.de>
2023-10-19 10:15:49 +0200

    Fix type issues in tags-with-autocomplete component

--- src/app/projects-view/tags-with-autocomplete/tags-with-autocomplete.component.ts
+++ src/app/projects-view/tags-with-autocomplete/tags-with-autocomplete.component.ts
@@ -30,7 +30,7 @@ export class TagsWithAutocompleteComponent implements OnInit {
   selectedTags: string[] = [];
   filteredTags: Observable<string[]>;
 
-  @ViewChild('auto') matAutocomplete: MatAutocomplete;
+  @ViewChild('auto') matAutocomplete!: MatAutocomplete;
 
   @Output('tags') tagsEmitter = new EventEmitter<string[]>();
   @Output() tagActionPrimary = new EventEmitter<string>();
