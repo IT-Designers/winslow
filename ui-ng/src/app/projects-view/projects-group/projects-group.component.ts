bb77edcdfb344ee6f279556b93f5e95d6760e285
Thorsten Rausch <thorsten.rausch@it-designers.de>
2023-10-23 10:58:07 +0200

    Fix makeImageBigger function

--- ui-ng/src/app/projects-view/projects-group/projects-group.component.ts
+++ ui-ng/src/app/projects-view/projects-group/projects-group.component.ts
@@ -44,8 +44,9 @@ export class ProjectsGroupComponent implements OnInit {
     return this.files.workspaceUrl(`${project.id}/output/thumbnail.jpg`);
   }
 
-  makeImageBigger(imageUrl: string, image: MouseEvent) {
-    if (image.target[`currentSrc`].includes('favicon.png')) {
+  makeImageBigger(imageUrl: string, event: MouseEvent) {
+    if (event.target instanceof HTMLImageElement && event.target.currentSrc.includes('favicon.png'))
+    {
       return;
     }
     this.dialog.image(imageUrl);
