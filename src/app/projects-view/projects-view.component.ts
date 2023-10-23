111ee4ad045583e4ed9257f8dfc82b5de62ccfdf
Eric Höfert <eric.hoefert@it-designers.de>
2023-10-23 15:19:18 +0200

    first fix for projects-view

--- src/app/projects-view/projects-view.component.ts
+++ src/app/projects-view/projects-view.component.ts
@@ -2,8 +2,6 @@ import {Component, EventEmitter, Input, OnInit, Output} from '@angular/core';
 import {ProjectGroup} from '../api/project-api.service';
 import {TagFilterComponent} from './tag-filter/tag-filter.component';
 import {ActivatedRoute, Router} from '@angular/router';
-import {FilesApiService} from '../api/files-api.service';
-import {DialogService} from '../dialog.service';
 import {ProjectInfo, StateInfo} from '../api/winslow-api';
 
 @Component({
@@ -13,13 +11,13 @@ import {ProjectInfo, StateInfo} from '../api/winslow-api';
 })
 export class ProjectsViewComponent implements OnInit {
 
-  @Input() stateInfo?: Map<string, StateInfo>;
-  @Input() selectedProject?: ProjectInfo;
-  @Input() projects!: ProjectInfo[];
-  @Input() projectsFiltered?: ProjectInfo[];
-  @Input() projectsGroups!: ProjectGroup[];
-  @Input() filter!: TagFilterComponent;
-  @Input() groupsOnTop?: boolean;
+  @Input() projects: ProjectInfo[];
+  @Input() projectsFiltered: ProjectInfo[];
+  @Input() projectsGroups: ProjectGroup[];
+  @Input() selectedProject: ProjectInfo;
+  @Input() stateInfo: Map<string, StateInfo>;
+  @Input() filter: TagFilterComponent;
+  @Input() groupsOnTop: boolean;
   documentGet = document;
 
   @Output() tagActionPrimary = new EventEmitter<string>();
@@ -27,8 +25,7 @@ export class ProjectsViewComponent implements OnInit {
 
   constructor(public route: ActivatedRoute,
               public router: Router,
-              private files: FilesApiService,
-              private dialog: DialogService) {
+  ) {
   }
 
   ngOnInit(): void {
@@ -40,15 +37,4 @@ export class ProjectsViewComponent implements OnInit {
     });
   }
 
-  thumbnailUrl(project: ProjectInfo) {
-    return this.files.workspaceUrl(`${project.id}/output/thumbnail.jpg`);
-  }
-
-  makeImageBigger(imageUrl: string, event: MouseEvent) {
-    if (event.target instanceof HTMLImageElement && event.target.currentSrc.includes('favicon.png'))
-    {
-      return;
-    }
-    this.dialog.image(imageUrl);
-  }
 }
