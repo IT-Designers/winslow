a13b84a0c06eb6c76192abdb1754795b7cf6d059
Thorsten Rausch <thorsten.rausch@it-designers.de>
2023-10-26 13:41:40 +0200

    Fix type issues in diagram-library and edit-forms components

--- src/app/pipeline-view/diagram-library/diagram-library.component.ts
+++ src/app/pipeline-view/diagram-library/diagram-library.component.ts
@@ -1,9 +1,11 @@
 import {Component, Input, OnInit, Output, EventEmitter, ViewChild} from '@angular/core';
 import {DiagramMakerNode} from "diagram-maker";
 import {
+  Raw,
   StageDefinitionInfo,
   StageDefinitionInfoUnion,
 } from "../../api/winslow-api";
+import {EditFormsComponent} from "./edit-forms/edit-forms.component";
 
 @Component({
   selector: 'app-diagram-library',
@@ -16,27 +18,31 @@ export class DiagramLibraryComponent implements OnInit {
   @Output() editNode = new EventEmitter();
   @Output() diagramApiCall = new EventEmitter();
   selectedNode$?: DiagramMakerNode<StageDefinitionInfo>;
-  savedData : boolean = true;
-  formObj : Object = {};
+  savedData: boolean = true;
+  formObj: Object = {};
 
-  @ViewChild('form') childForm;
+  @ViewChild('form') childForm?: EditFormsComponent;
 
   constructor() {
   }
+
   ngOnInit(): void {
     //console.log("Board Init")
   }
+
   @Input()
   set selectedNode(selectedNode: DiagramMakerNode<StageDefinitionInfo>) {
-      this.selectedNode$ = selectedNode;
-      this.formObj = {} as StageDefinitionInfoUnion;
-      this.formObj = JSON.parse(JSON.stringify(this.selectedNode$.consumerData));
+    this.selectedNode$ = selectedNode;
+    this.formObj = {} as StageDefinitionInfoUnion;
+    this.formObj = JSON.parse(JSON.stringify(this.selectedNode$.consumerData));
   }
+
   @Input()
-  set saveStatus(saveStatus : boolean){
+  set saveStatus(saveStatus: boolean) {
     this.savedData = saveStatus;
   };
-  onApiCall(action : String){       //used when clicking on the function icons e.g. save, undo...
+
+  onApiCall(action: String) {       //used when clicking on the function icons e.g. save, undo...
     switch (action) {
       case 'save':
         this.savedData = true;
@@ -44,13 +50,18 @@ export class DiagramLibraryComponent implements OnInit {
     }
     this.diagramApiCall.emit({action: action, node: this.selectedNode$});
   }
-  startSave(){          //starts the save on top level of the recursion of edit-forms
-    this.childForm.sendFormData();
+
+  startSave() {          //starts the save on top level of the recursion of edit-forms
+    this.childForm?.sendFormData();
   }
-  saveEdit(savedForm : Object){   //receives the chaneged data from the edit-forms and saves it in the board and in the node
-    this.formObj = savedForm[1] as StageDefinitionInfoUnion;
+
+  saveEdit(savedForm: Object) {   //receives the chaneged data from the edit-forms and saves it in the board and in the node
+    if (1 in savedForm) {
+      this.formObj = savedForm[1] as Raw<StageDefinitionInfoUnion>; //todo unsafe
+    }
     this.editNode.emit(this.formObj);
   }
+
   cancelEdit() {          //unloads the edit-node when clicking on the X-Icon
     this.selectedNode$ = undefined;
     this.resetSelectedNode.emit();
