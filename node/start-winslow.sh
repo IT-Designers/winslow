a9a3ea6fad7ee5ed3554103bbb61ccdd276c0265
Axel Schwab <ax4sc6@svm-0048.itd-intern.de>
2024-02-29 14:48:55 +0100

    make some cleaning for start script

--- node/start-winslow.sh
+++ node/start-winslow.sh
@@ -1,22 +1,27 @@
 #!/bin/bash
+#enable for debugging
+#set -x
+
+LOG_DATE_FORMAT="%m-%d %H:%M:%S"
+
 
 ##### config starts here
 
-#HTTP="80"
+HTTP="80"
 #HTTPS="443"
-#NODE_TYPE="executor"
-NODE_TYPE="observer"
+#NODE_TYPE="executor" #?
+NODE_TYPE="observer" #?
 STORAGE_TYPE="nfs"
-STORAGE_PATH="srv515.itd-intern.de:/data/streets/winslow"
+STORAGE_PATH="/var/nfs/winslow/workdir"
 #KEYSTORE_PATH_PKCS12="/root/winslow.itd-intern.de.p12"
 ADDITIONAL=""
 
 # for dev env
 ADDITIONAL+="-p 446:4646 "
-# ADDITIONAL+=" -e WINSLOW_DEV_ENV=true -e WINSLOW_DEV_REMOTE_USER=michael "
+ADDITIONAL+=" -e WINSLOW_DEV_ENV=true -e WINSLOW_DEV_REMOTE_USER=michael "
 
 # set this on the nfs-server!
-# ADDITIONAL+=" -e WINSLOW_NO_DIRTY_BYTES_ADJUSTMENT=1 "
+ADDITIONAL+=" -e WINSLOW_NO_DIRTY_BYTES_ADJUSTMENT=1 "
 
 # set this to disable auto-disabling Dirty-Bytes adjustment
 # IGNORE_NFSD=""
@@ -26,14 +31,15 @@ ADDITIONAL+="-p 446:4646 "
 
 PARAMS="$@"
 #IMAGE="winslow"
-IMAGE="repo.itd-intern.de/winslow/node"
+IMAGE="itdesigners1/winslow:2024.1-dev"
 NODE_NAME="$(hostname)"
 CONTAINER_NAME="winslow"
 GPUS="$(ls /dev/ | grep -i nvidia | wc -l)"
-WORKDIR="/winslow/"
+WORKDIR="/winslow/workdir"
 WEB_PORT="$HTTP"
 SUDO=""
 
+#only sudo when not root and not in docker group
 if [ "$(id -u)" -ne 0 ] && [ "$(id --name -G | grep -i docker | wc -l)" -eq 0 ]; then
     SUDO="sudo"
 fi
@@ -80,6 +86,7 @@ echo ""
 
 sleep 1
 
+# kill running container
 if [ $($SUDO docker ps -a --filter "name=$CONTAINER_NAME" | wc -l) -gt 1 ]; then
     echo " ::::: Stopping already running Winslow instance"
     $SUDO docker stop "$CONTAINER_NAME" > /dev/null
@@ -88,19 +95,19 @@ fi
 
 
 echo " ::::: Starting Winslow Container now"
-echo $SUDO docker run -itd --rm --privileged \
+echo $SUDO docker run --rm --privileged \
     --name "$CONTAINER_NAME" \
-    $(if [ "$GPUS" -gt 0 ]; then echo " --gpus all"; fi) \
-    $(if [ "$HTTP" != "" ] ; then echo " -p $HTTP:$WEB_PORT"; fi) \
-    $(if [ "$HTTPS" != "" ] ; then echo " -p $HTTPS:$WEB_PORT"; fi) \
-    $(if [ "$WEB_PORT" != "" ]; then echo "-e SERVER_PORT=$WEB_PORT"; fi) \
+    $([ "$GPUS" -gt 0 ] && echo " --gpus all") \
+    $([ "$HTTP" != "" ] && echo " -p $HTTP:$WEB_PORT") \
+    $([ "$HTTPS" != "" ] && echo " -p $HTTPS:$WEB_PORT") \
+    $([ "$WEB_PORT" != "" ] && echo "-e SERVER_PORT=$WEB_PORT") \
     $ADDITIONAL \
     -e WINSLOW_STORAGE_TYPE=$STORAGE_TYPE \
     -e WINSLOW_STORAGE_PATH=$STORAGE_PATH \
     -e WINSLOW_WORK_DIRECTORY=$WORKDIR \
     -e "WINSLOW_NODE_NAME=$NODE_NAME" \
-    $(if [ "$NODE_TYPE" == "observer" ]; then echo " -e WINSLOW_NO_STAGE_EXECUTION=1 "; fi) \
-    $(if [ "$STORAGE_TYPE" == "bind" ]; then echo " -v $STORAGE_PATH:/winslow"; fi) \
+    $([ "$NODE_TYPE" == "observer" ] && echo " -e WINSLOW_NO_STAGE_EXECUTION=1 ") \
+    $([ "$STORAGE_TYPE" == "bind" ] && echo " -v $STORAGE_PATH:/winslow") \
     -v /var/run/docker.sock:/var/run/docker.sock \
     $IMAGE \
     $PARAMS | bash
